0169d95a8ffda7c9fbe826be5e74cc6907a74b98
Vladimir Gorej <vladimir.gorej@gmail.com>
2017-05-13 16:24:24 +0200

    docs(cata): add more polymorphic documentation
    Closes #72

--- src/cata.js
+++ src/cata.js
@@ -1,39 +1,57 @@
-import { invoker, curry } from 'ramda';
+import { curry } from 'ramda';
 
 import isFunction from './isFunction';
 
 
-const cata = invoker(2, 'cata');
-const either = invoker(2, 'either');
-
+/* eslint-disable max-len */
 /**
- * The catamorphism for either. If the either is right than the right function will be executed with
- * the right value and the value of the function returned. Otherwise the left function
- * will be called with the left value.
+ * The catamorphism is a way of folding a type into a value.
+ *
+ * **Either**
+ *
+ * If the either is right than the right function will be executed with
+ * the `right` value and the value of the function returned. Otherwise the left function
+ * will be called with the `left` value.
+ *
+ * **Maybe**
+ *
+ * If the maybe is Some than the right function will be executed with the `some` value and the value of the function
+ * returned. Otherwise the left function with be called without an argument.
+ *
+ *
  *
  * @func cata
  * @memberOf RA
  * @since {@link https://char0n.github.io/ramda-adjunct/1.4.0|v1.4.0}
  * @category Function
- * @sig (a -> b) -> (a -> c) -> Either a -> b | c
+ * @sig (a -> b) -> (a -> c) -> Cata a -> b | c
  * @param {Function} leftFn The left function that consumes the left value
  * @param {Function} rightFn The right function that consumes the right value
- * @param {Either} either Either monad with catamorphic capabilities (`cata` or `either` method)
+ * @param {Cata} catamorphicObj Either, Maybe or any other type with catamorphic capabilities (`cata` or `either` method)
  * @return {*}
  * @see {@link https://cwmyers.github.io/monet.js/#cata|cata explained}
  * @example
  *
+ * // Either
  * const eitherR = Either.Right(1);
  * const eitherL = Either.Left(2);
  *
  * RA.cata(identity, identity, eitherR); //=> 1
  * RA.cata(identity, identity, eitherL); //=> 2
+ *
+ * // Maybe
+ * const maybeSome = Maybe.Some(1);
+ * const maybeNothing = Maybe.Nothing();
+ *
+ * RA.cata(identity, identity, maybeSome); //=> 1
+ * RA.cata(identity, identity, maybeNothing); //=> undefined
  */
+/* eslint-enable */
 const catamorphism = curry((leftFn, rightFn, catamorphicObj) => {
-  if (isFunction(catamorphicObj.either)) {
-    return either(leftFn, rightFn, catamorphicObj);
+  if (isFunction(catamorphicObj.cata)) {
+    return catamorphicObj.cata(leftFn, rightFn);
   }
-  return cata(leftFn, rightFn, catamorphicObj);
+  return catamorphicObj.either(leftFn, rightFn);
 });
 
 export default catamorphism;
