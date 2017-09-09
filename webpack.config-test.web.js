02ca26a0fd16f9e2be28b78a2e32aebd23691df6
Vladimir Gorej <vladimir.gorej@gmail.com>
2017-09-09 22:39:21 +0200

    test(index.d.ts): exclude from test:web
    Ref #55

--- webpack.config-test.web.js
+++ webpack.config-test.web.js
@@ -6,7 +6,9 @@ const path = require('path');
 
 module.exports = {
   target: 'web',
-  entry: glob.sync('./test/*.js'),
+  entry: glob.sync('./test/*.js', {
+    ignore: './test/typescript.js',
+  }),
   output: {
     path: path.resolve('.'),
     filename: 'tmp-test-bundle.js',
