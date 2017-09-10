d7ac0cbd1ff63a28f7cb2ca6485529a23788db62
Vladimir Gorej <vladimir.gorej@gmail.com>
2017-09-10 19:59:34 +0200

    test(index.d.ts): set timeout to max 15 seconds in test

--- test/typescript.js
+++ test/typescript.js
@@ -4,11 +4,8 @@ import * as tt from 'typescript-definition-tester';
 
 
 describe('Typescript definitions', function() {
-  beforeEach(function() {
-    this.timeout(15000);
-  });
-
   it('should compile against index.d.ts', (done) => {
+    this.timeout(15000); // give typescript compiler enough time to complete
     tt.compileDirectory(
       path.join(__dirname, '..', 'src'),
       test(/index.d.ts$/),
