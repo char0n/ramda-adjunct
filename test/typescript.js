5eb220114f0fe7571a47f1f9458e8cad703363d4
Vladimir Gorej <vladimir.gorej@gmail.com>
2017-09-10 20:03:28 +0200

    test(index.d.ts): use classical function declaration
    Use classical function delcaration intead of arrow.

--- test/typescript.js
+++ test/typescript.js
@@ -4,7 +4,7 @@ import * as tt from 'typescript-definition-tester';
 
 
 describe('Typescript definitions', function() {
-  it('should compile against index.d.ts', (done) => {
+  it('should compile against index.d.ts', function(done) {
     this.timeout(15000); // give typescript compiler enough time to complete
     tt.compileDirectory(
       path.join(__dirname, '..', 'src'),
