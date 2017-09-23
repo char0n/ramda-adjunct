e29900f942293fe911badf0a13248806cc204b1c
BjornMelgaard <melgaardbjorn@gmail.com>
2017-09-23 12:13:11 +0300

    params fix

--- src/mergeAllProps.js
+++ src/mergeAllProps.js
@@ -8,9 +8,9 @@ import { curry, mergeAll, props } from 'ramda';
  * @since {@link TODO}
  * @category Object
  * @sig [k] -> {k: {a}} -> {a}
- * @param {Array} ps The property names to fetch
- * @param {Array} obj The object to query
- * @return {Object} A merged properties of object.
+ * @param {!Array} ps The property names to fetch
+ * @param {!Object} obj The object to query
+ * @return {!Object} A merged properties of object.
  * @see {@link http://ramdajs.com/docs/#mergeAll|mergeAll} {@link http://ramdajs.com/docs/#props|props}
  * @example
  *
@@ -22,8 +22,6 @@ import { curry, mergeAll, props } from 'ramda';
  * const expected   = { fooinner: 1, barinner: 2 };
  * const withSpread = { ...obj.foo, ...obj.bar }
  * const withFunc   = R.mergeAllProps(['foo', 'bar'], obj)
-
- * @symb R.mergeAllProps([p1, p2], { p1: { a1: 1 }, p2: { a2: 2 } }) = { a1: 1, a2, 2 }
  */
 const mergeAllProps = curry((ps, object) => mergeAll(props(ps, object)));
 
