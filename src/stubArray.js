ba9c7025f32f22b02b3f4c99d17a91c9742338f3
BjornMelgaard <melgaardbjorn@gmail.com>
2017-11-26 21:32:12 +0200

    fix(#197): review

--- src/stubArray.js
+++ src/stubArray.js
@@ -1,5 +1,3 @@
-import { always } from 'ramda';
-
 /**
  * A function that returns new empty array on every call.
  *
@@ -7,13 +5,14 @@ import { always } from 'ramda';
  * @memberOf RA
  * @since {@link https://char0n.github.io/ramda-adjunct/2.1.0|v2.1.0}
  * @category Function
- * @sig ... -> []
- * @return {[]}
+ * @sig ... -> Array
+ * @return {Array} New empty array
  * @example
  *
  * RA.stubArray(); //=> []
  * RA.stubArray(1, 2, 3); //=> []
  */
-const stubArray = always([]);
+
+const stubArray = () => [];
 
 export default stubArray;
