bd62863e0c0f3c915c1a49f8daf91e732ad9a606
Vladimir Gorej <vladimir.gorej@gmail.com>
2017-12-01 23:19:47 +0100

    test(isThenable): make tests more declarative

--- test/isThenable.js
+++ test/isThenable.js
@@ -5,38 +5,48 @@ import args from './shared/arguments';
 
 
 describe('isThenable', function() {
-  it('tests a value for `thenable`', function() {
-    eq(RA.isThenable(args), false);
-    eq(RA.isThenable([1, 2, 3]), false);
-    eq(RA.isThenable(true), false);
-    eq(RA.isThenable(new Date()), false);
-    eq(RA.isThenable(new Error()), false);
-    eq(RA.isThenable(Array.prototype.slice), false);
-    eq(RA.isThenable({ 0: 1, length: 1 }), false);
-    eq(RA.isThenable(1), false);
-    eq(RA.isThenable(/x/), false);
-    eq(RA.isThenable(Symbol), false);
+  context('when value is not `thenable`', function() {
+    specify('should return false', function() {
+      eq(RA.isThenable(args), false);
+      eq(RA.isThenable([1, 2, 3]), false);
+      eq(RA.isThenable(true), false);
+      eq(RA.isThenable(new Date()), false);
+      eq(RA.isThenable(new Error()), false);
+      eq(RA.isThenable(Array.prototype.slice), false);
+      eq(RA.isThenable({ 0: 1, length: 1 }), false);
+      eq(RA.isThenable(1), false);
+      eq(RA.isThenable(/x/), false);
+      eq(RA.isThenable(Symbol), false);
+    });
   });
 
-  it('tests an object for `thenable`', function() {
-    eq(RA.isThenable({ then: () => {} }), true);
+  context('when value is thenable object', function() {
+    specify('should return true', function() {
+      eq(RA.isThenable({ then: () => {} }), true);
+    });
   });
 
-  it('tests an object prototype for `thenable`', function() {
-    const objWithPrototype = Object.create({ then: () => {} });
+  context("when value contains then method on it's prototype", function() {
+    specify('should return true', function() {
+      const objWithPrototype = Object.create({ then: () => {} });
 
-    eq(RA.isThenable(objWithPrototype), true);
+      eq(RA.isThenable(objWithPrototype), true);
+    });
   });
 
-  it('tests a promise for `thenable`', function() {
-    eq(RA.isThenable(Promise.resolve()), true);
-    eq(RA.isThenable(Promise.reject()), true);
+  context('when value is an instance of native Promise', function() {
+    specify('should return true', function() {
+      eq(RA.isThenable(Promise.resolve()), true);
+      eq(RA.isThenable(Promise.reject()), true);
+    });
   });
 
-  it('tests a function for `thenable`', function() {
-    const func = () => {};
-    func.then = () => {};
+  context('when value is a thenable function', function() {
+    specify('should return true', function() {
+      const func = () => {};
+      func.then = () => {};
 
-    eq(RA.isThenable(func), true);
+      eq(RA.isThenable(func), true);
+    });
   });
 });
