5cf85baa085742b44f642744b2a81f24b566af1e
Vladimir Gorej <vladimir.gorej@gmail.com>
2017-12-01 22:49:04 +0100

    feat: add isPromise
    Ref #35

--- src/index.js
+++ src/index.js
@@ -58,6 +58,7 @@ export { default as isEven } from './isEven';
 export { default as isPair } from './isPair';
 export { default as isNotPair } from './isNotPair';
 export { default as isThenable } from './isThenable';
+export { default as isPromise } from './isPromise';
 // Function
 export { default as stubUndefined } from './stubUndefined';
 export { default as stubNull } from './stubNull';
---
+++ src/isPromise.js
@@ -0,0 +1,30 @@
+import { both, pipe, toString, equals } from 'ramda';
+
+import isObj from './isObj';
+
+
+/**
+ * Checks if input value is a native `Promise`.
+ * The Promise object represents the eventual completion (or failure)
+ * of an asynchronous operation, and its resulting value.
+ *
+ * @func isPromise
+ * @memberOf RA
+ * @since {@link https://char0n.github.io/ramda-adjunct/2.1.0|v2.1.0}
+ * @category Type
+ * @sig * -> Boolean
+ * @param {*} val The value to test
+ * @return {Boolean}
+ * @see {@link https://promisesaplus.com/|Promises/A+}, {@link RA.isThenable|isThenable}
+ * @example
+ *
+ * RA.isPromise(null); // => false
+ * RA.isPromise(undefined); // => false
+ * RA.isPromise([]); // => false
+ * RA.isPromise(Promise.resolve()); // => true
+ * RA.isPromise(Promise.reject()); // => true
+ * RA.isPromise({ then: () => 1 }); // => false
+ */
+const isPromise = both(isObj, pipe(toString, equals('[object Promise]')));
+
+export default isPromise;
