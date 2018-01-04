a214f7583c6328ad210623d8c060951e5bfe6c03
Pedr Browne <pedr.browne@gmail.com>
2018-01-04 22:18:23 +0000

    feat(api): add notBoth (#260)
    Ref #234

---
+++ src/notBoth.js
@@ -0,0 +1,39 @@
+import { compose, complement, both } from 'ramda';
+
+
+/* eslint-disable max-len */
+/**
+ * A function which calls the two provided functions and returns the complement of `&&`ing the
+ * results.
+ * It returns true if the first function is false-y and the complement of the second function
+ * otherwise. Note that this is short-circuited, meaning that the second function will not be
+ * invoked if the first returns a false-y value. In short it will return true unless both predicates
+ * return true.
+ *
+ * In addition to functions, `RA.notBoth` also accepts any fantasy-land compatible
+ * applicative functor.
+ *
+ * @func notBoth
+ * @memberOf RA
+ * @since {@link https://char0n.github.io/ramda-adjunct/2.3.0|v2.3.0}
+ * @category Logic
+ * @sig (*... -> Boolean) -> (*... -> Boolean) -> (*... -> Boolean)
+ * @param {Function} f A predicate
+ * @param {Function} g Another predicate
+ * @return {Function} Returns a function that applies its arguments to `f` and `g` and returns the complement of `&&`ing their outputs together.
+ * @see {@link http://ramdajs.com/docs/#both|both}
+ * @example
+ *
+ * const gt10 = R.gt(R.__, 10)
+ * const even = (x) => x % 2 === 0;
+ * const f = RA.notBoth(gt10, even);
+ *
+ * f(12); //=> false
+ * f(8); //=> true
+ * f(11); //=> true
+ * f(9); //=> true
+ */
+/* eslint-enable max-len */
+const notBoth = compose(complement, both);
+
+export default notBoth;
---
+++ test/notBoth.js
@@ -0,0 +1,65 @@
+import { Just, Nothing } from 'monet';
+import sinon from 'sinon';
+
+import * as RA from '../src/index';
+import eq from './shared/eq';
+
+
+const supportsFantasyLand = () => {
+  try {
+    return RA.notBoth(Just(true), Just(true)).equals(Just(false));
+  } catch (e) {
+    return false;
+  }
+};
+
+describe('notBoth', function() {
+  let isFantasyLandSupported;
+  before(function() {
+    isFantasyLandSupported = supportsFantasyLand();
+  });
+
+  it('combines two boolean-returning functions into one', function() {
+    const even = x => x % 2 === 0;
+    const gt10 = x => x > 10;
+    const f = RA.notBoth(even, gt10);
+
+    eq(f(8), true);
+    eq(f(13), true);
+    eq(f(9), true);
+    eq(f(14), false);
+  });
+
+  it('accepts functions that take multiple parameters', function() {
+    const between = (a, b, c) => a < b && b < c;
+    const total20 = (a, b, c) => a + b + c === 20;
+    const f = RA.notBoth(between, total20);
+
+    eq(f(4, 5, 11), false);
+    eq(f(12, 2, 7), true);
+    eq(f(12, 2, 6), true);
+    eq(f(5, 6, 15), true);
+  });
+
+  it('does not evaluate the second expression if the first one is false', function() {
+    const f = () => false;
+    const z = sinon.spy();
+
+    RA.notBoth(f, z)();
+    eq(z.notCalled, true);
+  });
+
+  if (isFantasyLandSupported) {
+    it('accepts fantasy-land applicative functors', function() {
+      eq(RA.notBoth(Just(true), Just(true)), Just(false));
+      eq(RA.notBoth(Just(true), Just(false)), Just(true));
+      eq(RA.notBoth(Just(false), Just(true)), Just(true));
+      eq(RA.notBoth(Just(false), Just(false)), Just(true));
+      eq(RA.notBoth(Just(true), Nothing()), Nothing());
+      eq(RA.notBoth(Nothing(), Just(true)), Nothing());
+      eq(RA.notBoth(Nothing(), Just(false)), Nothing());
+      eq(RA.notBoth(Just(false), Nothing()), Nothing());
+      eq(RA.notBoth(Nothing(), Nothing()), Nothing());
+    });
+  }
+});
