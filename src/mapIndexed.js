788a9d777ca841605cfc4d5a402e732fe3db7b14
Guillaume ARM <trapcodien@hotmail.fr>
2018-02-10 23:45:26 +0100

    style(mapIndexed): some jsdoc styling
    Ref #345

--- src/mapIndexed.js
+++ src/mapIndexed.js
@@ -12,11 +12,11 @@ import { addIndex, map } from 'ramda';
  * @memberOf RA
  * @since {@link https://char0n.github.io/ramda-adjunct/2.5.0|v2.5.0}
  * @category List
- * @sig Functor f => ((a, Number, f a) => b) => f a -> f b
+ * @sig Functor f => ((a, Idx, f a) => b) => f a -> f b
  * @param {Function} fn The function to be called on every element of the input `list`
- * @param {Array} list The list to be iterated over.
- * @return {Array} The new list.
- * @see {@link http://ramdajs.com/docs/#addIndex|R.addIndex}, {@link http://ramdajs.com/docs/#map|R.map}.
+ * @param {Array} list The list to be iterated over
+ * @return {Array} The new list
+ * @see {@link http://ramdajs.com/docs/#addIndex|R.addIndex}, {@link http://ramdajs.com/docs/#map|R.map}
  * @example R.mapIndexed((val, idx, list) => idx + '-' + val, ['f', 'o', 'o', 'b', 'a', 'r']);
  * //=> ['0-f', '1-o', '2-o', '3-b', '4-a', '5-r']
  */
