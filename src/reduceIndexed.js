9f37b2995678f2f573c120fa793acfa73b72e48a
Guillaume ARM <trapcodien@hotmail.fr>
2018-02-11 11:52:09 +0100

    docs(reduceIndexed): some changes according to review
    Ref #357

--- src/reduceIndexed.js
+++ src/reduceIndexed.js
@@ -14,12 +14,16 @@ import { addIndex, reduce } from 'ramda';
  * @category List
  * @typedef Idx = Number
  * @sig ((a, b, Idx, [b]) => a) -> a -> [b] -> a
- * @param {Function} fn The function to be called on every element of the input `list`
+ * @param {Function} fn The iterator function. Receives four values,
+ * the accumulator, the current element from the array, index and the entire list
  * @param {*} acc The accumulator value
- * @param {Array} list The list iterate over
+ * @param {Array} list The list to iterate over
  * @return {*} The final, accumulated value
  * @see {@link http://ramdajs.com/docs/#addIndex|R.addIndex}, {@link http://ramdajs.com/docs/#reduce|R.reduce}
- * @example const initialList = ['f', 'o', 'o', 'b', 'a', 'r'];
+ * @example
+ *
+ * const initialList = ['f', 'o', 'o', 'b', 'a', 'r'];
+ *
  * reduceIndexed((acc, val, idx, list) => acc + '-' + val + idx, '', initialList);
  * //=> "-f0-o1-o2-b3-a4-r5"
  */
