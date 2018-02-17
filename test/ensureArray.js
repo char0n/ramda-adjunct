23871dea4b1e8e29ec55160fdd2cf22513b2d559
Guillaume ARM <trapcodien@gmail.com>
2018-02-17 13:02:13 +0100

    refactor(ensureArray): tests according to review

--- test/ensureArray.js
+++ test/ensureArray.js
@@ -1,21 +1,22 @@
-import chai from 'chai';
+import { assert } from 'chai';
 import * as RA from '../src/index';
 import eq from './shared/eq';
 
 
 describe('ensureArray', function () {
-  it('tests `Array` values', function () {
-    const emptyArray = [];
-    const filledArray = [42];
-    const arrayInstance = new Array();
+  const emptyArray = [];
+  const filledArray = [42];
+  const arrayInstance = new Array();
+  const arrayPrototype = Array.prototype;
 
-    chai.assert.strictEqual(RA.ensureArray(emptyArray), emptyArray);
-    chai.assert.strictEqual(RA.ensureArray(filledArray), filledArray);
-    chai.assert.strictEqual(RA.ensureArray(arrayInstance), arrayInstance);
-    chai.assert.strictEqual(RA.ensureArray(Array.prototype), Array.prototype);
+  it('tests `Array` values', function () {
+    assert.strictEqual(RA.ensureArray(emptyArray), emptyArray);
+    assert.strictEqual(RA.ensureArray(filledArray), filledArray);
+    assert.strictEqual(RA.ensureArray(arrayInstance), arrayInstance);
+    assert.strictEqual(RA.ensureArray(arrayPrototype), arrayPrototype);
   });
 
-  it('tests other values', function () {
+  it('returns non-Array values as the only element of a new Array', function () {
     eq(RA.ensureArray(void 0), [void 0]);
     eq(RA.ensureArray({}), [{}]);
     eq(RA.ensureArray(null), [null]);
@@ -24,6 +25,6 @@ describe('ensureArray', function () {
     eq(RA.ensureArray('Array'), ['Array']);
     eq(RA.ensureArray(true), [true]);
     eq(RA.ensureArray(false), [false]);
-    eq(RA.ensureArray({ __proto__: Array.prototype }), [{ __proto__: Array.prototype }]);
+    eq(RA.ensureArray({ __proto__: arrayPrototype }), [{ __proto__: arrayPrototype }]);
   });
 });
