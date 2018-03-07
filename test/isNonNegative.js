21e5c2ca83b459cdf4b0aeac41086d4a0d779dc6
Pedr Browne <pedr.browne@gmail.com>
2018-03-07 14:37:54 +0000

    feat: add isNonPositive and isNonNegative
    Add predicates for testing numbers for being non-positive (zero and all negative numbers) or non-negative (zero and all positive numbers)

---
+++ test/isNonNegative.js
@@ -0,0 +1,42 @@
+import * as RA from '../src/index';
+import MAX_SAFE_INTEGER from '../src/internal/polyfills/Number.MAX_SAFE_INTEGER';
+import MIN_SAFE_INTEGER from '../src/internal/polyfills/Number.MIN_SAFE_INTEGER';
+import eq from './shared/eq';
+import args from './shared/arguments';
+import Symbol from './shared/Symbol';
+
+describe('isNonNegative', function() {
+  it('tests a value for non-negative `Number`', function() {
+    eq(RA.isNonNegative(0), true);
+    eq(RA.isNonNegative(-0.1), false);
+    eq(RA.isNonNegative(0.1), true);
+    eq(RA.isNonNegative(Object(0)), true);
+    eq(RA.isNonNegative(Object(-0.1)), false);
+    eq(RA.isNonNegative(Object(0.1)), true);
+    eq(RA.isNonNegative(NaN), false);
+    eq(RA.isNonNegative(Infinity), true);
+    eq(RA.isNonNegative(-Infinity), false);
+    eq(RA.isNonNegative(MAX_SAFE_INTEGER), true);
+    eq(RA.isNonNegative(MIN_SAFE_INTEGER), false);
+    eq(RA.isNonNegative(Number.MAX_VALUE), true);
+    eq(RA.isNonNegative(Number.MIN_VALUE), true);
+
+    eq(RA.isNonNegative(new Date()), false);
+    eq(RA.isNonNegative(args), false);
+    eq(RA.isNonNegative([1, 2, 3]), false);
+    eq(RA.isNonNegative(Object(false)), false);
+    eq(RA.isNonNegative(new Error()), false);
+    eq(RA.isNonNegative(RA), false);
+    eq(RA.isNonNegative(Array.prototype.slice), false);
+    eq(RA.isNonNegative({ a: 1 }), false);
+    eq(RA.isNonNegative(/x/), false);
+    eq(RA.isNonNegative(Object('a')), false);
+
+    if (Symbol !== 'undefined') {
+      eq(RA.isNonNegative(Symbol), false);
+    }
+
+    eq(RA.isNonNegative(null), false);
+    eq(RA.isNonNegative(undefined), false);
+  });
+});
---
+++ test/isNonPositive.js
@@ -0,0 +1,42 @@
+import * as RA from '../src/index';
+import MAX_SAFE_INTEGER from '../src/internal/polyfills/Number.MAX_SAFE_INTEGER';
+import MIN_SAFE_INTEGER from '../src/internal/polyfills/Number.MIN_SAFE_INTEGER';
+import eq from './shared/eq';
+import args from './shared/arguments';
+import Symbol from './shared/Symbol';
+
+describe('isNonPositive', function() {
+  it('tests a value for positive `Number`', function() {
+    eq(RA.isNonPositive(0), true);
+    eq(RA.isNonPositive(-0.1), true);
+    eq(RA.isNonPositive(0.1), false);
+    eq(RA.isNonPositive(Object(0)), true);
+    eq(RA.isNonPositive(Object(-0.1)), true);
+    eq(RA.isNonPositive(Object(0.1)), false);
+    eq(RA.isNonPositive(NaN), false);
+    eq(RA.isNonPositive(Infinity), false);
+    eq(RA.isNonPositive(-Infinity), true);
+    eq(RA.isNonPositive(MAX_SAFE_INTEGER), false);
+    eq(RA.isNonPositive(MIN_SAFE_INTEGER), true);
+    eq(RA.isNonPositive(Number.MAX_VALUE), false);
+    eq(RA.isNonPositive(Number.MIN_VALUE), false);
+
+    eq(RA.isNonPositive(new Date()), false);
+    eq(RA.isNonPositive(args), false);
+    eq(RA.isNonPositive([1, 2, 3]), false);
+    eq(RA.isNonPositive(Object(false)), false);
+    eq(RA.isNonPositive(new Error()), false);
+    eq(RA.isNonPositive(RA), false);
+    eq(RA.isNonPositive(Array.prototype.slice), false);
+    eq(RA.isNonPositive({ a: 1 }), false);
+    eq(RA.isNonPositive(/x/), false);
+    eq(RA.isNonPositive(Object('a')), false);
+
+    if (Symbol !== 'undefined') {
+      eq(RA.isNonPositive(Symbol), false);
+    }
+
+    eq(RA.isNonPositive(null), false);
+    eq(RA.isNonPositive(undefined), false);
+  });
+});
