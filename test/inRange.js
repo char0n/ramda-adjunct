f2a9183c621aeb6e9009127ed3d11d5e0e706e02
Pedr Browne <pedr.browne@gmail.com>
2018-03-10 09:57:56 +0000

    test: feature detection for max/min safe integer

--- test/inRange.js
+++ test/inRange.js
@@ -1,7 +1,13 @@
 import chai from 'chai';
+import * as R from 'ramda';
 
 import * as RA from '../src/index';
 import eq from './shared/eq';
+
+const supportsMinMaxSafeInt = R.all(RA.isNotUndefined, [
+  Number.MAX_SAFE_INTEGER,
+  Number.MIN_SAFE_INTEGER,
+]);
 
 describe('inRange', function() {
   it('tests whether a value falls within supplied range', function() {
@@ -37,16 +43,6 @@ describe('inRange', function() {
     eq(f(Number.NEGATIVE_INFINITY), true);
   });
 
-  it('supports `Number.MAX_SAFE_INTEGER`', function() {
-    const f = RA.inRange(0, Number.MAX_SAFE_INTEGER);
-    eq(f(Number.MAX_SAFE_INTEGER), true);
-  });
-
-  it('supports `Number.MIN_SAFE_INTEGER`', function() {
-    const f = RA.inRange(Number.MIN_SAFE_INTEGER, 0);
-    eq(f(Number.MIN_SAFE_INTEGER), true);
-  });
-
   it('supports `Number.MIN_VALUE`', function() {
     const f = RA.inRange(0, Number.MIN_VALUE);
     eq(f(Number.MIN_VALUE), true);
@@ -63,4 +59,16 @@ describe('inRange', function() {
       return f(5);
     }, Error);
   });
+
+  if (supportsMinMaxSafeInt) {
+    it('supports `Number.MAX_SAFE_INTEGER`', function() {
+      const f = RA.inRange(0, Number.MAX_SAFE_INTEGER);
+      eq(f(Number.MAX_SAFE_INTEGER), true);
+    });
+
+    it('supports `Number.MIN_SAFE_INTEGER`', function() {
+      const f = RA.inRange(Number.MIN_SAFE_INTEGER, 0);
+      eq(f(Number.MIN_SAFE_INTEGER), true);
+    });
+  }
 });
