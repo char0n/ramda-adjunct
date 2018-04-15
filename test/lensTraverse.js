594f6de8b0df7148138ff8ee2286e2d9e4a491b7
Vladimir Gorej <vladimir.gorej@gmail.com>
2018-04-15 13:41:01 +0200

    refactor(lensTraverse): finish the feature

--- test/lensTraverse.js
+++ test/lensTraverse.js
@@ -4,7 +4,7 @@ import { assert } from 'chai';
 import * as RA from '../src';
 
 describe('lensTraverse', function() {
-  it('should operate on a list of applicatives', function() {
+  it('should operate on a list of Applicatives', function() {
     const result = R.over(RA.lensTraverse(RA.Identity.of), R.map(R.add(1)), [
       RA.Identity.of(2),
       RA.Identity.of(3),
@@ -14,7 +14,15 @@ describe('lensTraverse', function() {
     assert.deepEqual(result.value, expected.value);
   });
 
-  it('should travers left to right', function() {
+  it('should operate on list of  primitives (numbers)', function() {
+    const travFn = x => RA.Identity.of(x + 1);
+    const result = R.over(RA.lensTraverse(RA.Identity.of), travFn, [2, 3]);
+    const expected = RA.Identity.of([3, 4]);
+
+    assert.deepEqual(result.value, expected.value);
+  });
+
+  it('should travers Applicatives left to right', function() {
     const result = R.over(RA.lensTraverse(RA.Identity.of), R.identity, [
       RA.Identity.of(2),
       RA.Identity.of(3),
@@ -22,6 +30,12 @@ describe('lensTraverse', function() {
     const expected = RA.Identity.of([2, 3]);
 
     assert.deepEqual(result.value, expected.value);
+  });
+
+  it('should throw on traversing on non Applicatives (e.g. numbers)', function() {
+    const fn = R.over(RA.lensTraverse(RA.Identity.of), R.identity);
+
+    assert.throws(() => fn([2, 3]), TypeError);
   });
 
   context('should dispatch to sequence function', function() {
@@ -56,14 +70,22 @@ describe('lensTraverse', function() {
     assert.deepEqual(result, expected);
   });
 
-  it('should be inspectable via view', function() {
-    const result = R.view(RA.lensTraverse(RA.Identity.of), [
-      RA.Identity.of(2),
-      RA.Identity.of(3),
-    ]);
-    const expected = RA.Identity.of([2, 3]);
+  context('view', function() {
+    specify('should work on list of Applicables', function() {
+      const result = R.view(RA.lensTraverse(RA.Identity.of), [
+        RA.Identity.of(2),
+        RA.Identity.of(3),
+      ]);
+      const expected = RA.Identity.of([2, 3]);
 
-    assert.deepEqual(result, expected);
+      assert.deepEqual(result, expected);
+    });
+
+    specify('should throw on list of non Applicables', function() {
+      const fn = R.view(RA.lensTraverse(RA.Identity.of));
+
+      assert.throws(() => fn([2, 3]));
+    });
   });
 
   it('should be modifiable via set', function() {
@@ -74,15 +96,5 @@ describe('lensTraverse', function() {
     const expected = RA.Identity.of([2, 2]);
 
     assert.deepEqual(result, expected);
-  });
-
-  it('should be curried', function() {
-    const toFunctorFn = x => RA.Identity.of(R.map(R.add(1), x));
-    const target = [RA.Identity.of(1)];
-
-    assert.isObject(RA.lensTraverse(RA.Identity.of)(toFunctorFn)(target));
-    assert.isObject(RA.lensTraverse(RA.Identity.of, toFunctorFn)(target));
-    assert.isObject(RA.lensTraverse(RA.Identity.of)(toFunctorFn, target));
-    assert.isObject(RA.lensTraverse(RA.Identity.of, toFunctorFn, target));
   });
 });
