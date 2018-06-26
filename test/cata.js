f4945d73dd15c9733401df15436b71b7b1188789
Vladimir Gorej <vladimir.gorej@gmail.com>
2018-06-26 22:42:26 +0200

    test(cata): make test consistent

--- test/cata.js
+++ test/cata.js
@@ -1,5 +1,5 @@
 import { Either } from 'monet';
-import { identity } from 'ramda';
+import * as R from 'ramda';
 import { assert } from 'chai';
 
 import * as RA from '../src/index';
@@ -9,40 +9,43 @@ describe('cata', function() {
   const eitherR = Either.Right(1);
   const eitherL = Either.Left(2);
 
-  it('tests an Either for catamorphism', function() {
-    eq(RA.cata(identity, identity, eitherR), 1);
-    eq(RA.cata(identity, identity, eitherL), 2);
+  it('should test for Either catamorphism', function() {
+    eq(RA.cata(R.identity, R.identity, eitherR), 1);
+    eq(RA.cata(R.identity, R.identity, eitherL), 2);
   });
 
-  it('test currying', function() {
-    eq(RA.cata(identity)(identity)(eitherR), 1);
-    eq(RA.cata(identity, identity)(eitherL), 2);
+  it('should curry', function() {
+    eq(RA.cata(R.identity)(R.identity)(eitherR), 1);
+    eq(RA.cata(R.identity, R.identity)(eitherL), 2);
   });
 
-  it('tests left for catamorphism without functions', function() {
-    assert.throws(RA.cata.bind(null, null, identity, eitherL), TypeError);
-    eq(RA.cata(identity, null, eitherL), 2);
+  it('should test Left for catamorphism without function', function() {
+    assert.throws(RA.cata.bind(null, null, R.identity, eitherL), TypeError);
+    eq(RA.cata(R.identity, null, eitherL), 2);
   });
 
-  it('tests right for catamorphism without functions', function() {
-    assert.throws(RA.cata.bind(null, identity, null, eitherR), TypeError);
-    eq(RA.cata(null, identity, eitherR), 1);
+  it('should test Right for catamorphism without function', function() {
+    assert.throws(RA.cata.bind(null, R.identity, null, eitherR), TypeError);
+    eq(RA.cata(null, R.identity, eitherR), 1);
   });
 
-  it('test catamorphism on either method', function() {
+  it('should call either method on catamorphic object', function() {
     const eitherRWithEither = Either.Right(1);
     const eitherLWithEither = Either.Left(2);
 
     eitherRWithEither.either = eitherRWithEither.cata;
     eitherLWithEither.either = eitherLWithEither.cata;
 
-    eq(RA.cata(identity, identity, eitherRWithEither), 1);
-    eq(RA.cata(identity, identity, eitherLWithEither), 2);
+    eq(RA.cata(R.identity, R.identity, eitherRWithEither), 1);
+    eq(RA.cata(R.identity, R.identity, eitherLWithEither), 2);
   });
 
-  it('tests on monad without catamorphic behavior', function() {
-    assert.throws(RA.cata.bind(null, identity, identity, {}), TypeError);
-    assert.throws(RA.cata.bind(null, identity, identity, null), TypeError);
-    assert.throws(RA.cata.bind(null, identity, identity, undefined), TypeError);
+  it('should throw Error on monad without catamorphic behavior', function() {
+    assert.throws(RA.cata.bind(null, R.identity, R.identity, {}), TypeError);
+    assert.throws(RA.cata.bind(null, R.identity, R.identity, null), TypeError);
+    assert.throws(
+      RA.cata.bind(null, R.identity, R.identity, undefined),
+      TypeError
+    );
   });
 });
