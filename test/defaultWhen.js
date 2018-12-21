f377333333bd5b032eafe07cfbcbef9b95f5a9f0
Vladimir Gorej <vladimir.gorej@gmail.com>
2018-12-21 15:53:41 +0100

    test(defaultWhen): make tests consistent

--- test/defaultWhen.js
+++ test/defaultWhen.js
@@ -2,17 +2,17 @@ import * as RA from '../src/index';
 import eq from './shared/eq';
 
 describe('defaultWhen', function() {
-  it('tests returning default value', function() {
+  it('should return default value', function() {
     eq(RA.defaultWhen(RA.isNull, 1, null), 1);
     eq(RA.defaultWhen(RA.isString, 2, ''), 2);
   });
 
-  it('tests returning value', function() {
+  it('should return value', function() {
     eq(RA.defaultWhen(RA.isNull, 1, undefined), undefined);
     eq(RA.defaultWhen(RA.isString, 2, 3), 3);
   });
 
-  it('tests currying', function() {
+  it('should curry', function() {
     eq(RA.defaultWhen(RA.isNull)(1)(null), 1);
     eq(RA.defaultWhen(RA.isNull, 1)(null), 1);
     eq(RA.defaultWhen(RA.isNull, 1, null), 1);
