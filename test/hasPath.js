35f0e7d34b31aa277df6b2e9748f22bebdc19ea8
Vladimir Gorej <vladimir.gorej@gmail.com>
2018-12-21 16:13:02 +0100

    test(hasPath): make tests consistent

--- test/hasPath.js
+++ test/hasPath.js
@@ -2,13 +2,13 @@ import * as RA from '../src/index';
 import eq from './shared/eq';
 
 describe('hasPath', function() {
-  it('tests for paths in objects', function() {
+  it('should work on paths in objects', function() {
     eq(RA.hasPath(['a', 'b'], { a: { b: 1 } }), true);
     eq(RA.hasPath(['a', 'b', 'c'], { a: { b: 1 } }), false);
     eq(RA.hasPath(['a', 'b'], { a: {} }), false);
   });
 
-  it('tests for paths in arrays', function() {
+  it('should work on arrays', function() {
     eq(RA.hasPath([0], [1, 2]), true);
     eq(RA.hasPath([2], [1, 2]), false);
 
@@ -16,16 +16,16 @@ describe('hasPath', function() {
     eq(RA.hasPath(['2'], [1, 2]), false);
   });
 
-  it('tests empty path', function() {
+  it('should return false for empty paths', function() {
     eq(RA.hasPath([], { a: {} }), false);
   });
 
-  it('tests paths on non-objects', function() {
+  it('should return false on non-objects', function() {
     eq(RA.hasPath(['a', 'b'], undefined), false);
     eq(RA.hasPath(['a', 'b'], null), false);
   });
 
-  it('tests currying', function() {
+  it('should curry', function() {
     eq(RA.hasPath(['a', 'b'])({ a: { b: 1 } }), true);
   });
 });
