ec09c4c8d0a6663b31e11ffbca7b1f3886d5d6a9
Vladimir Gorej <vladimir.gorej@gmail.com>
2018-12-30 11:38:57 +0100

    feat(isNaN): add support for ramda placeholder
    Also make tests consistent.
    
    Ref #624

--- test/isNaN.js
+++ test/isNaN.js
@@ -1,9 +1,11 @@
+import * as R from 'ramda';
+
 import * as RA from '../src';
 import polyfill from '../src/internal/polyfills/Number.isNaN';
 import eq from './shared/eq';
 
 describe('isNaN', function() {
-  it('tests a value for `NaN`', function() {
+  it('should test value for a `NaN`', function() {
     eq(RA.isNaN(NaN), true);
     eq(RA.isNaN(Number.NaN), true);
     eq(RA.isNaN(0 / 0), true);
@@ -24,7 +26,7 @@ describe('isNaN', function() {
     eq(RA.isNaN(' '), false);
   });
 
-  it('tests polyfill for `NaN', function() {
+  it('should test polyfill for a `NaN', function() {
     eq(polyfill(NaN), true);
     eq(polyfill(Number.NaN), true);
     eq(polyfill(0 / 0), true);
@@ -43,5 +45,11 @@ describe('isNaN', function() {
     eq(polyfill('37.37'), false);
     eq(polyfill(''), false);
     eq(polyfill(' '), false);
+  });
+
+  it('should support placeholder to specify "gaps"', function() {
+    const isNaN = RA.isNaN(R.__);
+
+    eq(isNaN(NaN), true);
   });
 });
