87f97def18d1f27c94d92474fa2ec1e1bd1b21c5
Vladimir Gorej <vladimir.gorej@gmail.com>
2019-04-27 16:04:57 +0200

    feat(isNonNegative): add support for Ramda placeholder
    Also make tests consistent.
    
    Ref #624

--- test/isNonNegative.js
+++ test/isNonNegative.js
@@ -1,42 +1,57 @@
+import { assert } from 'chai';
+import * as R from 'ramda';
+
 import * as RA from '../src';
 import MAX_SAFE_INTEGER from '../src/internal/polyfills/Number.MAX_SAFE_INTEGER';
 import MIN_SAFE_INTEGER from '../src/internal/polyfills/Number.MIN_SAFE_INTEGER';
-import eq from './shared/eq';
 import args from './shared/arguments';
 import Symbol from './shared/Symbol';
 
 describe('isNonNegative', function() {
-  it('tests a value for non-negative `Number`', function() {
-    eq(RA.isNonNegative(0), true);
-    eq(RA.isNonNegative(-0.1), false);
-    eq(RA.isNonNegative(0.1), true);
-    eq(RA.isNonNegative(Object(0)), true);
-    eq(RA.isNonNegative(Object(-0.1)), false);
-    eq(RA.isNonNegative(Object(0.1)), true);
-    eq(RA.isNonNegative(NaN), false);
-    eq(RA.isNonNegative(Infinity), true);
-    eq(RA.isNonNegative(-Infinity), false);
-    eq(RA.isNonNegative(MAX_SAFE_INTEGER), true);
-    eq(RA.isNonNegative(MIN_SAFE_INTEGER), false);
-    eq(RA.isNonNegative(Number.MAX_VALUE), true);
-    eq(RA.isNonNegative(Number.MIN_VALUE), true);
+  context('given non negative number', function() {
+    specify('should return true', function() {
+      assert.isTrue(RA.isNonNegative(0));
+      assert.isTrue(RA.isNonNegative(0.1));
+      assert.isTrue(RA.isNonNegative(Object(0)));
+      assert.isTrue(RA.isNonNegative(Object(0.1)));
+      assert.isTrue(RA.isNonNegative(Infinity));
+      assert.isTrue(RA.isNonNegative(MAX_SAFE_INTEGER));
+      assert.isTrue(RA.isNonNegative(Number.MAX_VALUE));
+      assert.isTrue(RA.isNonNegative(Number.MIN_VALUE));
+    });
+  });
 
-    eq(RA.isNonNegative(new Date()), false);
-    eq(RA.isNonNegative(args), false);
-    eq(RA.isNonNegative([1, 2, 3]), false);
-    eq(RA.isNonNegative(Object(false)), false);
-    eq(RA.isNonNegative(new Error()), false);
-    eq(RA.isNonNegative(RA), false);
-    eq(RA.isNonNegative(Array.prototype.slice), false);
-    eq(RA.isNonNegative({ a: 1 }), false);
-    eq(RA.isNonNegative(/x/), false);
-    eq(RA.isNonNegative(Object('a')), false);
+  context('given negative number or any other value', function() {
+    specify('should return false', function() {
+      assert.isFalse(RA.isNonNegative(-0.1));
+      assert.isFalse(RA.isNonNegative(Object(-0.1)));
+      assert.isFalse(RA.isNonNegative(NaN));
+      assert.isFalse(RA.isNonNegative(-Infinity));
+      assert.isFalse(RA.isNonNegative(MIN_SAFE_INTEGER));
 
-    if (Symbol !== 'undefined') {
-      eq(RA.isNonNegative(Symbol), false);
-    }
+      assert.isFalse(RA.isNonNegative(new Date()));
+      assert.isFalse(RA.isNonNegative(args));
+      assert.isFalse(RA.isNonNegative([1, 2, 3]));
+      assert.isFalse(RA.isNonNegative(Object(false)));
+      assert.isFalse(RA.isNonNegative(new Error()));
+      assert.isFalse(RA.isNonNegative(RA));
+      assert.isFalse(RA.isNonNegative(Array.prototype.slice));
+      assert.isFalse(RA.isNonNegative({ a: 1 }));
+      assert.isFalse(RA.isNonNegative(/x/));
+      assert.isFalse(RA.isNonNegative(Object('a')));
 
-    eq(RA.isNonNegative(null), false);
-    eq(RA.isNonNegative(undefined), false);
+      if (Symbol !== 'undefined') {
+        assert.isFalse(RA.isNonNegative(Symbol));
+      }
+
+      assert.isFalse(RA.isNonNegative(null));
+      assert.isFalse(RA.isNonNegative(undefined));
+    });
+  });
+
+  it('should support placeholder to specify "gaps"', function() {
+    const isNonNegative = RA.isNonNegative(R.__);
+
+    assert.isTrue(isNonNegative(1));
   });
 });
