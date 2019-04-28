c5749ecdd8ce4456ec3971f4ddc0630fc90bae78
Vladimir Gorej <vladimir.gorej@gmail.com>
2019-04-28 10:56:17 +0200

    test(isNotArrayLike): make tests consistent
    Ref #624

--- test/isNotArrayLike.js
+++ test/isNotArrayLike.js
@@ -1,47 +1,67 @@
+import * as R from 'ramda';
+import { assert } from 'chai';
+
 import * as RA from '../src';
-import eq from './shared/eq';
 
 describe('isArrayLike', function() {
-  it('is false for Arrays', function() {
-    eq(RA.isNotArrayLike([]), false);
-    eq(RA.isNotArrayLike([1, 2, 3, 4]), false);
-    eq(RA.isNotArrayLike([null]), false);
+  context('given array', function() {
+    specify('should return false', function() {
+      assert.isFalse(RA.isNotArrayLike([]));
+      assert.isFalse(RA.isNotArrayLike([1, 2, 3, 4]));
+      assert.isFalse(RA.isNotArrayLike([null]));
+    });
   });
 
-  it('is false for arguments', function() {
-    function testingFn() {
-      return RA.isNotArrayLike(arguments);
-    }
-    eq(testingFn(), false);
-    eq(testingFn(1, 2, 3), false);
-    eq(testingFn(null), false);
+  context('given arguments object', function() {
+    specify('should return false', function() {
+      function testingFn() {
+        return RA.isNotArrayLike(arguments);
+      }
+      assert.isFalse(testingFn());
+      assert.isFalse(testingFn(1, 2, 3));
+      assert.isFalse(testingFn(null));
+    });
   });
 
-  it('is true for Strings', function() {
-    eq(RA.isNotArrayLike(''), true);
-    eq(RA.isNotArrayLike('abcdefg'), true);
+  context('given string', function() {
+    specify('should return true', function() {
+      assert.isTrue(RA.isNotArrayLike(''));
+      assert.isTrue(RA.isNotArrayLike('abcdefg'));
+    });
   });
 
-  it('is false for arbitrary objects with numeric length, if extreme indices are defined', function() {
-    const obj1 = { length: 0 };
-    const obj2 = { 0: 'something', length: 0 };
-    const obj3 = { 0: void 0, length: 0 };
-    const obj4 = { 0: 'zero', 1: 'one', length: 2 };
-    const obj5 = { 0: 'zero', length: 2 };
-    const obj6 = { 1: 'one', length: 2 };
-    eq(RA.isNotArrayLike(obj1), false);
-    eq(RA.isNotArrayLike(obj2), false);
-    eq(RA.isNotArrayLike(obj3), false);
-    eq(RA.isNotArrayLike(obj4), false);
-    eq(RA.isNotArrayLike(obj5), true);
-    eq(RA.isNotArrayLike(obj6), true);
+  context('given arbitrary objects with numeric length', function() {
+    context('and extreme indices are defined', function() {
+      const obj1 = { length: 0 };
+      const obj2 = { 0: 'something', length: 0 };
+      const obj3 = { 0: void 0, length: 0 };
+      const obj4 = { 0: 'zero', 1: 'one', length: 2 };
+      const obj5 = { 0: 'zero', length: 2 };
+      const obj6 = { 1: 'one', length: 2 };
+
+      assert.isFalse(RA.isNotArrayLike(obj1));
+      assert.isFalse(RA.isNotArrayLike(obj2));
+      assert.isFalse(RA.isNotArrayLike(obj3));
+      assert.isFalse(RA.isNotArrayLike(obj4));
+
+      assert.isTrue(RA.isNotArrayLike(obj5));
+      assert.isTrue(RA.isNotArrayLike(obj6));
+    });
   });
 
-  it('is true for everything else', function() {
-    eq(RA.isNotArrayLike(undefined), true);
-    eq(RA.isNotArrayLike(1), true);
-    eq(RA.isNotArrayLike({}), true);
-    eq(RA.isNotArrayLike(false), true);
-    eq(RA.isNotArrayLike(function() {}), true);
+  context('given array like value or anything else', function() {
+    specify('should return true', function() {
+      assert.isTrue(RA.isNotArrayLike(undefined));
+      assert.isTrue(RA.isNotArrayLike(1));
+      assert.isTrue(RA.isNotArrayLike({}));
+      assert.isTrue(RA.isNotArrayLike(false));
+      assert.isTrue(RA.isNotArrayLike(function() {}));
+    });
+  });
+
+  it('should support placeholder to specify "gaps"', function() {
+    const isNotArrayLike = RA.isNotArrayLike(R.__);
+
+    assert.isTrue(isNotArrayLike(-1));
   });
 });
