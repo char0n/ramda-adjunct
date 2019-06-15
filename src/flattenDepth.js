5b702ffc9ec8ab7ce7a4271d0e35e4af57de911e
Vladimir Gorej <vladimir.gorej@gmail.com>
2019-06-15 12:37:57 +0200

    test(flattenDepth): make tests more declarative
    Ref #844

--- src/flattenDepth.js
+++ src/flattenDepth.js
@@ -11,7 +11,7 @@ const flatten1 = _makeFlat(false);
  * @since {@link https://char0n.github.io/ramda-adjunct/2.19.0|v2.19.0}
  * @category List
  * @sig Number -> [a] -> [b]
- * @param {!number} depth The maximum recursion depth.
+ * @param {!number} depth The maximum recursion depth
  * @param {!Array} list The array to flatten
  * @return {!Array} Returns the new flattened array
  * @see {@link http://ramdajs.com/docs/#flatten|R.flatten}, {@link http://ramdajs.com/docs/#unnest|R.unnest}
@@ -20,11 +20,11 @@ const flatten1 = _makeFlat(false);
  * RA.flattenDepth(
  *   2,
  *   [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10]
- * ); // => [1, 2, 3, 4, 5, 6, [[7], 8], 9, 10];
+ * ); //=> [1, 2, 3, 4, 5, 6, [[7], 8], 9, 10];
  */
 const flattenDepth = curry((depth, list) => {
   let currentDept = depth;
-  let flatList = list;
+  let flatList = [...list];
 
   while (currentDept > 0) {
     flatList = flatten1(flatList);
