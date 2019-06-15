ad5983f4758da5cdff7b163753134c08b4f5e49b
Vladimir Gorej <vladimir.gorej@gmail.com>
2019-06-15 12:37:57 +0200

    test(flattenDepth): make tests more declarative
    Ref #844

--- test/flattenDepth.js
+++ test/flattenDepth.js
@@ -4,69 +4,126 @@ import * as RA from '../src';
 
 describe('flattenDepth', function() {
   context('given a list that has nested elements', function() {
-    specify(
-      'should return a flattened list to the specified depth',
-      function() {
-        const nest = [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10];
+    context('and the depth is positive integer number', function() {
+      specify('should flatten the list according the the depth', function() {
+        const depth = 2;
+        const actual = [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10];
+        const expected = [1, 2, 3, 4, 5, 6, [[7], 8], 9, 10];
 
-        assert.deepEqual(
-          [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10],
-          RA.flattenDepth(-1, nest)
-        );
-        assert.deepEqual(
-          [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10],
-          RA.flattenDepth(0, nest)
-        );
-        assert.deepEqual(
-          [1, 2, 3, [4, 5], 6, [[[7], 8]], 9, 10],
-          RA.flattenDepth(1, nest)
-        );
-        assert.deepEqual(
-          [1, 2, 3, 4, 5, 6, [[7], 8], 9, 10],
-          RA.flattenDepth(2, nest)
-        );
-        assert.deepEqual(
-          [1, 2, 3, 4, 5, 6, [7], 8, 9, 10],
-          RA.flattenDepth(3, nest)
-        );
-        assert.deepEqual(
-          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-          RA.flattenDepth(4, nest)
-        );
-        assert.deepEqual(
-          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-          RA.flattenDepth(5, nest)
-        );
+        assert.deepEqual(RA.flattenDepth(depth, actual), expected);
+      });
+    });
 
-        assert.deepEqual(
-          [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10],
-          RA.flattenDepth(-1)(nest)
-        );
-        assert.deepEqual(
-          [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10],
-          RA.flattenDepth(0)(nest)
-        );
-        assert.deepEqual(
-          [1, 2, 3, [4, 5], 6, [[[7], 8]], 9, 10],
-          RA.flattenDepth(1)(nest)
-        );
-        assert.deepEqual(
-          [1, 2, 3, 4, 5, 6, [[7], 8], 9, 10],
-          RA.flattenDepth(2)(nest)
-        );
-        assert.deepEqual(
-          [1, 2, 3, 4, 5, 6, [7], 8, 9, 10],
-          RA.flattenDepth(3)(nest)
-        );
-        assert.deepEqual(
-          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-          RA.flattenDepth(4)(nest)
-        );
-        assert.deepEqual(
-          [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-          RA.flattenDepth(5)(nest)
-        );
-      }
-    );
+    context('and the depth is negative integer number', function() {
+      specify('should return shallow copy of the list', function() {
+        const depth = -2;
+        const actual = [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10];
+        const expected = RA.flattenDepth(depth, actual);
+
+        assert.deepEqual(actual, expected);
+        assert.notStrictEqual(actual, expected);
+      });
+    });
+
+    context('and the depth is 0', function() {
+      specify('should return shallow copy of the list', function() {
+        const depth = 0;
+        const actual = [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10];
+        const expected = RA.flattenDepth(depth, actual);
+
+        assert.deepEqual(actual, expected);
+        assert.notStrictEqual(actual, expected);
+      });
+    });
+
+    context('and the depth is arbitrary positive integer number', function() {
+      specify('should flatten list', function() {
+        const list = [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10];
+
+        assert.deepEqual(RA.flattenDepth(1, list), [
+          1,
+          2,
+          3,
+          [4, 5],
+          6,
+          [[[7], 8]],
+          9,
+          10,
+        ]);
+        assert.deepEqual(RA.flattenDepth(2, list), [
+          1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          [[7], 8],
+          9,
+          10,
+        ]);
+        assert.deepEqual(RA.flattenDepth(3, list), [
+          1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          [7],
+          8,
+          9,
+          10,
+        ]);
+        assert.deepEqual(RA.flattenDepth(4, list), [
+          1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          7,
+          8,
+          9,
+          10,
+        ]);
+        assert.deepEqual(RA.flattenDepth(5, list), [
+          1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          7,
+          8,
+          9,
+          10,
+        ]);
+      });
+    });
+
+    context('and the depth is positive float number', function() {
+      specify('should ceil depth to positive integer number', function() {
+        const depth = 2.2;
+        const actual = [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10];
+        const expected = [1, 2, 3, 4, 5, 6, [7], 8, 9, 10];
+
+        assert.deepEqual(RA.flattenDepth(depth, actual), expected);
+      });
+    });
+
+    context('and the depth is negative float number', function() {
+      specify('should return shallow copy of the list', function() {
+        const depth = -2.2;
+        const actual = [1, [2], [3, [4, 5], 6, [[[7], 8]]], 9, 10];
+        const expected = RA.flattenDepth(depth, actual);
+
+        assert.deepEqual(actual, expected);
+        assert.notStrictEqual(actual, expected);
+      });
+    });
+  });
+
+  it('should curry', function() {
+    assert.deepEqual(RA.flattenDepth(0, []), []);
+    assert.deepEqual(RA.flattenDepth(0)([]), []);
   });
 });
