7f3dbe77af106011fd08b2c888bc4f0d5bcb6c5c
Vladimir Gorej <vladimir.gorej@gmail.com>
2019-07-21 16:14:09 +0200

    test(nand): make tests and documentation consistent
    Ref #624

--- test/nand.js
+++ test/nand.js
@@ -1,38 +1,33 @@
-import eq from './shared/eq';
+import { assert } from 'chai';
+
 import * as RA from '../src';
 
-describe('RA.nand', function() {
+describe('nand', function() {
   it('should work on booleans', function() {
-    eq(RA.nand(true, true), false);
-    eq(RA.nand(false, true), true);
-    eq(RA.nand(true, false), true);
-    eq(RA.nand(false, false), true);
+    assert.isFalse(RA.nand(true, true));
+    assert.isTrue(RA.nand(false, true));
+    assert.isTrue(RA.nand(true, false));
+    assert.isTrue(RA.nand(false, false));
   });
 
   it('should work on numbers', function() {
-    eq(RA.nand(1.0, 1.0), false);
-    eq(RA.nand(0.0, 1.0), true);
-    eq(RA.nand(1.0, 0.0), true);
-    eq(RA.nand(0.0, 0.0), true);
+    assert.isFalse(RA.nand(1.0, 1.0));
+    assert.isTrue(RA.nand(0.0, 1.0));
+    assert.isTrue(RA.nand(1.0, 0.0));
+    assert.isTrue(RA.nand(0.0, 0.0));
   });
 
-  it('should work on a combination of things', function() {
-    eq(RA.nand(true, 1.0), false);
-    eq(RA.nand(null, true), true);
-    eq(RA.nand(1.0, undefined), true);
-    eq(RA.nand(0.0, false), true);
-    eq(RA.nand(null, null), true);
-    eq(RA.nand(null, undefined), true);
+  it('should work on a combination of different values', function() {
+    assert.isFalse(RA.nand(true, 1.0));
+    assert.isTrue(RA.nand(null, true));
+    assert.isTrue(RA.nand(1.0, undefined));
+    assert.isTrue(RA.nand(0.0, false));
+    assert.isTrue(RA.nand(null, null));
+    assert.isTrue(RA.nand(null, undefined));
   });
 
   it('should support currying', function() {
-    eq(RA.nand(true, true), false);
-    eq(RA.nand(true)(true), false);
-    eq(RA.nand(false, true), true);
-    eq(RA.nand(false)(true), true);
-    eq(RA.nand(true, false), true);
-    eq(RA.nand(true)(false), true);
-    eq(RA.nand(false, false), true);
-    eq(RA.nand(false)(false), true);
+    assert.isFalse(RA.nand(true, true));
+    assert.isFalse(RA.nand(true)(true));
   });
 });
