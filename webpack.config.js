9f43ec4d95588def8ed995445385ffdf64a39eb0
Vladimir Gorej <vladimir.gorej@gmail.com>
2019-08-12 11:30:21 +0200

    chore(package): update eslint-config-airbnb-base to version 14.0.0
    Closes #1013

--- webpack.config.js
+++ webpack.config.js
@@ -34,275 +34,263 @@ const minimizeTrait = {
   },
 };
 
-const ra = Object.assign(
-  {
-    mode: 'production',
-    entry: './src/index.js',
-    target: 'node',
-    output: {
-      path: path.resolve('./dist'),
-      filename: 'RA.node.js',
-      libraryTarget: 'umd',
-      library: 'RA',
-    },
-    externals: {
-      ramda: 'ramda',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js)$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: {
-                    node: '4',
-                  },
-                  forceAllTransforms: true,
-                },
-              ],
-            ],
-            plugins: [
-              [
-                '@babel/plugin-transform-modules-commonjs',
-                {
-                  loose: true,
-                },
-              ],
-            ],
-          },
-        },
-      ],
-    },
+const ra = {
+  mode: 'production',
+  entry: './src/index.js',
+  target: 'node',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'RA.node.js',
+    libraryTarget: 'umd',
+    library: 'RA',
   },
-  nonMinimizeTrait
-);
+  externals: {
+    ramda: 'ramda',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  node: '4',
+                },
+                forceAllTransforms: true,
+              },
+            ],
+          ],
+          plugins: [
+            [
+              '@babel/plugin-transform-modules-commonjs',
+              {
+                loose: true,
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+  ...nonMinimizeTrait,
+};
 
-const raMin = Object.assign(
-  {
-    mode: 'production',
-    entry: './src/index.js',
-    target: 'node',
-    output: {
-      path: path.resolve('./dist'),
-      filename: 'RA.node.min.js',
-      libraryTarget: 'umd',
-      library: 'RA',
-    },
-    externals: {
-      ramda: 'ramda',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js)$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: {
-                    node: '4',
-                  },
-                  forceAllTransforms: true,
-                },
-              ],
-            ],
-            plugins: [
-              [
-                '@babel/plugin-transform-modules-commonjs',
-                {
-                  loose: true,
-                },
-              ],
-            ],
-          },
-        },
-      ],
-    },
+const raMin = {
+  mode: 'production',
+  entry: './src/index.js',
+  target: 'node',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'RA.node.min.js',
+    libraryTarget: 'umd',
+    library: 'RA',
   },
-  minimizeTrait
-);
+  externals: {
+    ramda: 'ramda',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  node: '4',
+                },
+                forceAllTransforms: true,
+              },
+            ],
+          ],
+          plugins: [
+            [
+              '@babel/plugin-transform-modules-commonjs',
+              {
+                loose: true,
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+  ...minimizeTrait,
+};
 
-const raWeb = Object.assign(
-  {
-    mode: 'production',
-    entry: './src/index.js',
-    target: 'web',
-    output: {
-      path: path.resolve('./dist'),
-      filename: 'RA.web.js',
-      libraryTarget: 'umd',
-      library: 'RA',
-    },
-    externals: {
-      ramda: 'R',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js)$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  forceAllTransforms: true,
-                },
-              ],
-            ],
-            plugins: [
-              [
-                '@babel/plugin-transform-modules-commonjs',
-                {
-                  loose: true,
-                },
-              ],
-            ],
-          },
-        },
-      ],
-    },
+const raWeb = {
+  mode: 'production',
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'RA.web.js',
+    libraryTarget: 'umd',
+    library: 'RA',
   },
-  nonMinimizeTrait
-);
+  externals: {
+    ramda: 'R',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                forceAllTransforms: true,
+              },
+            ],
+          ],
+          plugins: [
+            [
+              '@babel/plugin-transform-modules-commonjs',
+              {
+                loose: true,
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+  ...nonMinimizeTrait,
+};
 
-const raWebMin = Object.assign(
-  {
-    mode: 'production',
-    entry: './src/index.js',
-    target: 'web',
-    output: {
-      path: path.resolve('./dist'),
-      filename: 'RA.web.min.js',
-      libraryTarget: 'umd',
-      library: 'RA',
-    },
-    externals: {
-      ramda: 'R',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js)$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  forceAllTransforms: true,
-                },
-              ],
-            ],
-            plugins: [
-              [
-                '@babel/plugin-transform-modules-commonjs',
-                {
-                  loose: true,
-                },
-              ],
-            ],
-          },
-        },
-      ],
-    },
+const raWebMin = {
+  mode: 'production',
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'RA.web.min.js',
+    libraryTarget: 'umd',
+    library: 'RA',
   },
-  minimizeTrait
-);
+  externals: {
+    ramda: 'R',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                forceAllTransforms: true,
+              },
+            ],
+          ],
+          plugins: [
+            [
+              '@babel/plugin-transform-modules-commonjs',
+              {
+                loose: true,
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+  ...minimizeTrait,
+};
 
-const raWebStandalone = Object.assign(
-  {
-    mode: 'production',
-    entry: './src/index.js',
-    target: 'web',
-    output: {
-      path: path.resolve('./dist'),
-      filename: 'RA.web.standalone.js',
-      libraryTarget: 'umd',
-      library: 'RA',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js)$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  forceAllTransforms: true,
-                },
-              ],
-            ],
-            plugins: [
-              [
-                '@babel/plugin-transform-modules-commonjs',
-                {
-                  loose: true,
-                },
-              ],
-            ],
-          },
-        },
-      ],
-    },
+const raWebStandalone = {
+  mode: 'production',
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'RA.web.standalone.js',
+    libraryTarget: 'umd',
+    library: 'RA',
   },
-  nonMinimizeTrait
-);
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                forceAllTransforms: true,
+              },
+            ],
+          ],
+          plugins: [
+            [
+              '@babel/plugin-transform-modules-commonjs',
+              {
+                loose: true,
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+  ...nonMinimizeTrait,
+};
 
-const rawWebStandaloneMin = Object.assign(
-  {
-    mode: 'production',
-    entry: './src/index.js',
-    target: 'web',
-    output: {
-      path: path.resolve('./dist'),
-      filename: 'RA.web.standalone.min.js',
-      libraryTarget: 'umd',
-      library: 'RA',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js)$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  forceAllTransforms: true,
-                },
-              ],
-            ],
-            plugins: [
-              [
-                '@babel/plugin-transform-modules-commonjs',
-                {
-                  loose: true,
-                },
-              ],
-            ],
-          },
-        },
-      ],
-    },
+const rawWebStandaloneMin = {
+  mode: 'production',
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'RA.web.standalone.min.js',
+    libraryTarget: 'umd',
+    library: 'RA',
   },
-  minimizeTrait
-);
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                forceAllTransforms: true,
+              },
+            ],
+          ],
+          plugins: [
+            [
+              '@babel/plugin-transform-modules-commonjs',
+              {
+                loose: true,
+              },
+            ],
+          ],
+        },
+      },
+    ],
+  },
+  ...minimizeTrait,
+};
 
 module.exports = [
   ra,
