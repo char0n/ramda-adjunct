3dd7d9459305277dbdf6ac7d84ec5a825d385154
Vladimir Gorej <vladimir.gorej@gmail.com>
2019-09-05 14:17:52 +0200

    refactor(nor): align this feature with CR comments
    Closes #285

--- src/nor.js
+++ src/nor.js
@@ -5,21 +5,22 @@ import { or, complement } from 'ramda';
  *
  * @func nor
  * @memberOf RA
- * @since {@link https://char0n.github.io/ramda-adjunct/2.19.0|v2.19.0}
+ * @since {@link https://char0n.github.io/ramda-adjunct/2.20.0|v2.20.0}
  * @category Logic
- * @sig a -> b -> Boolean
+ * @sig a -> b -> a ⊽ b
  * @param {*} a
  * @param {*} b
- * @return {Boolean} true if both arguments are falsy
+ * @return {boolean} true if both arguments are falsy
+ * @see {@link RA.neither|neither}
  * @example
  *
  * RA.nor(true, true); //=> false
  * RA.nor(false, true); //=> false
  * RA.nor(true, false); //=> false
  * RA.nor(false, false); //=> true
- * RA.nor(1.0, 1.0); //=> false
- * RA.nor(1.0, 0); //=> false
- * RA.nor(0, 1.0); //=> false
+ * RA.nor(1, 1); //=> false
+ * RA.nor(1, 0); //=> false
+ * RA.nor(0, 1); //=> false
  * RA.nor(0, 0); //=> true
  */
 const nor = complement(or); // eslint-disable-line ramda/complement-simplification
