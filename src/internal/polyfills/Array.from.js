8485b2a1603c0a8c49d7c3c1a9577277e00456b0
Rafael <rafaelpadovezi@gmail.com>
2019-10-05 03:00:23 -0300

    refactor: simplify function

--- src/internal/polyfills/Array.from.js
+++ src/internal/polyfills/Array.from.js
@@ -1,6 +1,12 @@
 import isIterable from '../../isIterable';
 import isUndefined from '../../isUndefined';
 
+const copyArray = (items, mapFn, thisArg) => {
+  if (isUndefined(mapFn)) return [...items];
+  if (isUndefined(thisArg)) return [...items].map(mapFn);
+  return [...items].map(mapFn).bind(thisArg);
+};
+
 const fromArray = (items, mapFn = undefined, thisArg = undefined) => {
   if (items == null) {
     throw new TypeError(
@@ -9,9 +15,7 @@ const fromArray = (items, mapFn = undefined, thisArg = undefined) => {
   }
 
   if (isIterable(items)) {
-    if (isUndefined(mapFn)) return [...items];
-    if (isUndefined(thisArg)) return [...items].map(mapFn);
-    return [...items].map(mapFn).bind(thisArg);
+    return copyArray(items, mapFn, thisArg);
   }
 
   return [];
