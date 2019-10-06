435d3651ea8e09793bb1bbdcde643a631ad19fb5
Vladimir Gorej <vladimir.gorej@gmail.com>
2019-10-06 12:13:51 +0200

    test(argsPass): add tests for typescript typings
    Ref #978

---
+++ types/test/argsPass.ts
@@ -0,0 +1,31 @@
+import * as RA from 'ramda-adjunct';
+
+/**
+ * Helpers.
+ */
+
+const combiningPredicate = <T>(predicateFn: Function) => (iterable: T[]): boolean =>
+    iterable.reduce((acc: boolean, val: T) => acc && predicateFn(val), true);
+
+const nonBoolPredicate = (val: string): string => val;
+
+const nonCurriedCombiningPredicate = <T>(predicateFn: Function, iterable: T[]): boolean =>
+    iterable.reduce((acc: boolean, val: T) => acc && predicateFn(val), true);
+
+/**
+ * Tests.
+ */
+
+RA.argsPass(combiningPredicate, [Boolean, Boolean])(1, 2); // $ExpectType boolean
+RA.argsPass(combiningPredicate)([Boolean, Boolean])(1, 2); // $ExpectType boolean
+RA.argsPass(combiningPredicate, [Boolean, Boolean])('a', 'b'); // $ExpectType boolean
+RA.argsPass(combiningPredicate)([Boolean, Boolean])('a', 'b'); // $ExpectType boolean
+RA.argsPass(combiningPredicate, [Boolean, Boolean])('a', 2); // $ExpectType boolean
+RA.argsPass(combiningPredicate)([Boolean, Boolean])('a', 2); // $ExpectType boolean
+RA.argsPass(combiningPredicate, [Boolean, Boolean])(); // $ExpectType boolean
+
+RA.argsPass(combiningPredicate)([Boolean, Boolean])(); // $ExpectType boolean
+
+RA.argsPass(nonCurriedCombiningPredicate, [Boolean, Boolean])(1, 2); // $ExpectError
+
+RA.argsPass(combiningPredicate, [nonBoolPredicate])(1, 2); // $ExpectError
