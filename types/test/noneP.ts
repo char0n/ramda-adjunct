846dd4c595603932b9619ef1834c959b922fc8f2
Will Caulfield <34461499+willcaul@users.noreply.github.com>
2019-10-06 18:52:42 -0400

    test: update types test

--- types/test/noneP.ts
+++ types/test/noneP.ts
@@ -2,7 +2,8 @@ import * as RA from 'ramda-adjunct';
 
 import { customIterable } from './helpers';
 
-RA.noneP([1, 2]); // $ExpectType Promise<number[]>
+RA.noneP([Promise.resolve(1), 2, RA.resolveP(3)]); // $ExpectType Promise<number[]>
+RA.noneP([Promise.reject<number>(1)]); // $ExpectType Promise<number[]>
 RA.noneP(['a', 'b']); // $ExpectType Promise<string[]>
 RA.noneP([1, 'a']); // $ExpectType Promise<(string | number)[]>
 RA.noneP([]); // $ExpectType Promise<never[]>
