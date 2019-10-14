337c9d36f839a960e43ff014084ea84b7486da89
Alex <js636f@gmail.com>
2019-10-14 10:34:31 +0300

    feat(type): isPositiveZero (#1111)
    Closes #1111 1108

---
+++ test/isPositiveZero.js
@@ -0,0 +1,39 @@
+import { assert } from 'chai';
+import * as R from 'ramda';
+
+import * as RA from '../src';
+
+describe('isPositiveZero', function() {
+  context('given a positive zero (+0) or (0)', function() {
+    specify('should return true', function() {
+      assert.isTrue(RA.isPositiveZero(+0));
+      assert.isTrue(RA.isPositiveZero(0));
+    });
+  });
+
+  context('given a value different from positive zero (+0)', function() {
+    specify('should return false', function() {
+      assert.isFalse(RA.isPositiveZero(-0));
+      assert.isFalse(RA.isPositiveZero(null));
+      assert.isFalse(RA.isPositiveZero(true));
+      assert.isFalse(RA.isPositiveZero(NaN));
+      assert.isFalse(RA.isPositiveZero({}));
+      assert.isFalse(RA.isPositiveZero([]));
+      assert.isFalse(RA.isPositiveZero(() => {}));
+      assert.isFalse(RA.isPositiveZero('string'));
+      assert.isFalse(RA.isPositiveZero(1));
+      assert.isFalse(RA.isPositiveZero(0.1));
+      assert.isFalse(RA.isPositiveZero(Object(0.1)));
+      assert.isFalse(RA.isPositiveZero(NaN));
+      assert.isFalse(RA.isPositiveZero(Infinity));
+      assert.isFalse(RA.isPositiveZero(Number.MAX_VALUE));
+      assert.isFalse(RA.isPositiveZero(Number.MIN_VALUE));
+    });
+  });
+
+  it('should support placeholder to specify "gaps"', function() {
+    const isPositiveZero = RA.isPositiveZero(R.__);
+
+    assert.isTrue(isPositiveZero(+0));
+  });
+});
