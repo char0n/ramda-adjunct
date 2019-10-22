ef20ce624a0f00be6e51787328c2edeed9ec5042
thomasmichaelwallace <thomasmichaelwallace@gmail.com>
2019-10-22 11:50:11 +0100

    feat(object): add unzipObjWith
    Closes #801

--- types/test/unzipObjWith.ts
+++ types/test/unzipObjWith.ts
@@ -2,7 +2,7 @@ import * as RA from 'ramda-adjunct';
 
 RA.unzipObjWith((v, k) => [k, 1], { a: 1 }); // $ExpectType [string[], number[]]
 RA.unzipObjWith((v, k) => [2, 'hello'])({ a: 1 }); // $ExpectType [number[], string[]]
-RA.unzipObjWith((v, k) => [k, v], {}); // $ExpectType [string[], unknown[]]
+RA.unzipObjWith((v, k) => [k, 1], {}); // $ExpectType [string[], number[]]
 RA.unzipObjWith<string, boolean, number>((v, k) => [true, 5], { a: 'b' }); // $ExpectType [boolean[], number[]]
 
 RA.unzipObjWith((v, k) => true, { a: 1 }); // $ExpectError
