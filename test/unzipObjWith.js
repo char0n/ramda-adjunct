64ce9b6e454072973e1ca0e9151e86279a669340
thomas michael wallace <thomasmichaelwallace@gmail.com>
2019-10-23 08:57:50 +0100

    feat(object): add unzipObjWith (#1130)
    Closes #801

---
+++ test/unzipObjWith.js
@@ -0,0 +1,51 @@
+import { assert } from 'chai';
+import * as R from 'ramda';
+
+import * as RA from '../src';
+
+describe('unzipObjWith', function() {
+  it('should unzip an object into pair of lists', function() {
+    const fn = (value, key) => [key, { ...value, name: key }];
+    const obj = {
+      packageA: { version: '1.0.9' },
+      packageB: { version: '1.6.1' },
+      packageC: { version: '1.6.0' },
+    };
+
+    const actual = RA.unzipObjWith(fn, obj);
+
+    const expected = [
+      ['packageA', 'packageB', 'packageC'],
+      [
+        { name: 'packageA', version: '1.0.9' },
+        { name: 'packageB', version: '1.6.1' },
+        { name: 'packageC', version: '1.6.0' },
+      ],
+    ];
+
+    assert.deepEqual(actual, expected);
+  });
+
+  context('given empty object', function() {
+    specify('should return a pair of empty arrays', function() {
+      const fn = (value, key) => [key, value];
+      const obj = {};
+
+      const actual = RA.unzipObjWith(fn, obj);
+      const expected = [[], []];
+
+      assert.deepEqual(actual, expected);
+    });
+  });
+
+  it('should curry', function() {
+    const fn = (v, k) => [`new${k.toUpperCase()}`, 2 * v];
+    const obj = { a: 1, b: 2, c: 3 };
+    const expected = [['newA', 'newB', 'newC'], [2, 4, 6]];
+
+    assert.deepEqual(RA.unzipObjWith(fn, obj), expected);
+    assert.deepEqual(RA.unzipObjWith(fn)(obj), expected);
+    assert.deepEqual(RA.unzipObjWith()(fn)(obj), expected);
+    assert.deepEqual(RA.unzipObjWith(R.__, obj)(fn), expected);
+  });
+});
