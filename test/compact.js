21ccd40c1d91ae6f378bdbd6a82eba25bad3db53
Alex <js636f@gmail.com>
2019-10-26 19:50:36 +0300

    test: use chai asserts instead of internal eq function (#1144)
    Ref #895

--- test/compact.js
+++ test/compact.js
@@ -2,18 +2,20 @@ import { assert } from 'chai';
 import * as R from 'ramda';
 
 import * as RA from '../src';
-import eq from './shared/eq';
 
 describe('compact', function() {
   context('given list contains falsy values', function() {
     specify('should filter them', function() {
-      eq(RA.compact([0, 1, false, 2, '', 3, null, undefined, NaN]), [1, 2, 3]);
+      assert.sameOrderedMembers(
+        RA.compact([0, 1, false, 2, '', 3, null, undefined, NaN]),
+        [1, 2, 3]
+      );
     });
   });
 
   context('given list contains truthy values only', function() {
     specify('should not filter anything', function() {
-      eq(RA.compact([1, 2, 3]), [1, 2, 3]);
+      assert.sameOrderedMembers(RA.compact([1, 2, 3]), [1, 2, 3]);
     });
 
     specify('should return new list with exactly same items', function() {
@@ -34,6 +36,6 @@ describe('compact', function() {
   it('should support placeholder to specify "gaps"', function() {
     const compact = RA.compact(R.__);
 
-    eq(compact([]), []);
+    assert.sameOrderedMembers(compact([]), []);
   });
 });
--- test/concatAll.js
+++ test/concatAll.js
@@ -3,24 +3,23 @@ import { NEL, Nil } from 'monet';
 import * as R from 'ramda';
 
 import * as RA from '../src';
-import eq from './shared/eq';
 
 describe('concatAll', function() {
   it('should concatenate arrays', function() {
-    eq(RA.concatAll([[1], [2], [3]]), [1, 2, 3]);
+    assert.sameOrderedMembers(RA.concatAll([[1], [2], [3]]), [1, 2, 3]);
   });
 
   it('should concatenate strings', function() {
-    eq(RA.concatAll(['1', '2', '3']), '123');
+    assert.strictEqual(RA.concatAll(['1', '2', '3']), '123');
   });
 
   it('should concatenate semigroups', function() {
-    eq(RA.concatAll([NEL(1), NEL(2)]), NEL(1, NEL(2, Nil)));
+    assert.deepEqual(RA.concatAll([NEL(1), NEL(2)]), NEL(1, NEL(2, Nil)));
   });
 
   context('given foldable is empty', function() {
     specify('should returns undefined', function() {
-      eq(RA.concatAll([]), undefined);
+      assert.isUndefined(RA.concatAll([]));
     });
   });
 
@@ -48,6 +47,6 @@ describe('concatAll', function() {
   it('should support placeholder to specify "gaps"', function() {
     const concatAll = RA.concatAll(R.__);
 
-    eq(concatAll(['1', '2', '3']), '123');
+    assert.strictEqual(concatAll(['1', '2', '3']), '123');
   });
 });
