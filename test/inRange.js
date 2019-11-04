3bf33bc6c0cc9d538541da6fc96b1f66dcf251df
Alex <js636f@gmail.com>
2019-11-04 23:48:49 +0200

    test: use chai directly instead of internal eq function (#1156)
    Ref #895

--- test/inRange.js
+++ test/inRange.js
@@ -1,7 +1,6 @@
 import { assert } from 'chai';
 
 import * as RA from '../src';
-import eq from './shared/eq';
 import MIN_SAFE_INTEGER from '../src/internal/polyfills/Number.MIN_SAFE_INTEGER';
 import MAX_SAFE_INTEGER from '../src/internal/polyfills/Number.MAX_SAFE_INTEGER';
 
@@ -9,60 +8,60 @@ describe('inRange', function() {
   context('given integer falls within supplied range', function() {
     specify('should return true', function() {
       const f = RA.inRange(5, 10);
-      eq(f(5), true);
-      eq(f(9), true);
-      eq(f(10), false);
-      eq(f(4), false);
-      eq(f(11), false);
+      assert.isTrue(f(5));
+      assert.isTrue(f(9));
+      assert.isFalse(f(10));
+      assert.isFalse(f(4));
+      assert.isFalse(f(11));
     });
   });
 
   context('given float falls within supplied range', function() {
     specify('should return true', function() {
       const f = RA.inRange(5.5, 10.5);
-      eq(f(5.5), true);
-      eq(f(9.5), true);
-      eq(f(10.5), false);
-      eq(f(4.5), false);
-      eq(f(11.5), false);
+      assert.isTrue(f(5.5));
+      assert.isTrue(f(9.5));
+      assert.isFalse(f(10.5));
+      assert.isFalse(f(4.5));
+      assert.isFalse(f(11.5));
     });
   });
 
   it('should support negative values', function() {
     const f = RA.inRange(-10, -5);
-    eq(f(-6), true);
-    eq(f(-5), false);
-    eq(f(-10), true);
-    eq(f(-4), false);
-    eq(f(-11), false);
+    assert.isTrue(f(-6));
+    assert.isFalse(f(-5));
+    assert.isTrue(f(-10));
+    assert.isFalse(f(-4));
+    assert.isFalse(f(-11));
   });
 
   context('given NAN is used', function() {
     it('should return false', function() {
-      eq(RA.inRange(0, NaN, 5), false);
-      eq(RA.inRange(NaN, 10, 5), false);
-      eq(RA.inRange(5, 10, NaN), false);
+      assert.isFalse(RA.inRange(0, NaN, 5));
+      assert.isFalse(RA.inRange(NaN, 10, 5));
+      assert.isFalse(RA.inRange(5, 10, NaN));
     });
   });
 
   it('should support `Number.POSITIVE_INFINITY`', function() {
     const f = RA.inRange(0, Number.POSITIVE_INFINITY);
-    eq(f(Number.MAX_VALUE), true);
+    assert.isTrue(f(Number.MAX_VALUE));
   });
 
   it('should support `Number.NEGATIVE_INFINITY`', function() {
     const f = RA.inRange(Number.NEGATIVE_INFINITY, 0);
-    eq(f(Number.NEGATIVE_INFINITY), true);
+    assert.isTrue(f(Number.NEGATIVE_INFINITY));
   });
 
   it('should support `Number.MIN_VALUE`', function() {
     const f = RA.inRange(Number.MIN_VALUE, 1);
-    eq(f(Number.MIN_VALUE), true);
+    assert.isTrue(f(Number.MIN_VALUE));
   });
 
   it('should support `Number.MAX_VALUE`', function() {
     const f = RA.inRange(0, Number.MAX_VALUE);
-    eq(f(Number.MAX_VALUE), false);
+    assert.isFalse(f(Number.MAX_VALUE));
   });
 
   context('given `low` value is greater than `high` value', function() {
@@ -81,17 +80,17 @@ describe('inRange', function() {
 
   it('should support `Number.MAX_SAFE_INTEGER`', function() {
     const f = RA.inRange(0, MAX_SAFE_INTEGER);
-    eq(f(MAX_SAFE_INTEGER), false);
+    assert.isFalse(f(MAX_SAFE_INTEGER));
   });
 
   it('should support `Number.MIN_SAFE_INTEGER`', function() {
     const f = RA.inRange(MIN_SAFE_INTEGER, 0);
-    eq(f(MIN_SAFE_INTEGER), true);
+    assert.isTrue(f(MIN_SAFE_INTEGER));
   });
 
   it('should support currying', function() {
-    eq(RA.inRange(5)(10)(5), true);
-    eq(RA.inRange(5, 10)(5), true);
-    eq(RA.inRange(5)(10, 5), true);
+    assert.isTrue(RA.inRange(5)(10)(5));
+    assert.isTrue(RA.inRange(5, 10)(5));
+    assert.isTrue(RA.inRange(5)(10, 5));
   });
 });
