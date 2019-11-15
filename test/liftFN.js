b2d8b0f1ce4a5eabf5ff5c3cde6794b8a3a83fd2
js636f <js636f@gmail.com>
2019-11-15 19:07:26 +0200

    Using R.equals

--- test/liftFN.js
+++ test/liftFN.js
@@ -18,9 +18,11 @@ describe('liftFN', function() {
   });
 
   it('limits a variadic function to the specified arity', function() {
-    assert.deepEqual(
-      addN3(Maybe.Some(1), Maybe.Some(1), Maybe.Some(1)),
-      Maybe.Some(3)
+    assert.isTrue(
+      R.equals(
+        addN3(Maybe.Some(1), Maybe.Some(1), Maybe.Some(1)),
+        Maybe.Some(3)
+      )
     );
   });
 
@@ -38,43 +40,59 @@ describe('liftFN', function() {
   });
 
   it('can lift functions of any arity', function() {
-    assert.deepEqual(
-      addN3(Maybe.Some(1), Maybe.Some(1), Maybe.Some(1)),
-      Maybe.Some(3)
+    assert.isTrue(
+      R.equals(
+        addN3(Maybe.Some(1), Maybe.Some(1), Maybe.Some(1)),
+        Maybe.Some(3)
+      )
     );
-    assert.deepEqual(
-      addN4(Maybe.Some(1), Maybe.Some(1), Maybe.Some(1), Maybe.Some(1)),
-      Maybe.Some(4)
+    assert.isTrue(
+      R.equals(
+        addN4(Maybe.Some(1), Maybe.Some(1), Maybe.Some(1), Maybe.Some(1)),
+        Maybe.Some(4)
+      )
     );
-    assert.deepEqual(
-      addN5(
-        Maybe.Some(1),
-        Maybe.Some(1),
-        Maybe.Some(1),
-        Maybe.Some(1),
-        Maybe.Some(1)
-      ),
-      Maybe.Some(5)
+    assert.isTrue(
+      R.equals(
+        addN5(
+          Maybe.Some(1),
+          Maybe.Some(1),
+          Maybe.Some(1),
+          Maybe.Some(1),
+          Maybe.Some(1)
+        ),
+        Maybe.Some(5)
+      )
     );
   });
 
   it('retain order of arguments', function() {
-    assert.deepEqual(
-      RA.liftFN(3, add3)(Maybe.Some('a'), Maybe.Some('b'), Maybe.Some('c')),
-      Maybe.Some('abc')
+    assert.isTrue(
+      R.equals(
+        RA.liftFN(3, add3)(Maybe.Some('a'), Maybe.Some('b'), Maybe.Some('c')),
+        Maybe.Some('abc')
+      )
     );
-    assert.deepEqual(
-      RA.liftFN(3, add3)(Identity.of('a'), Identity.of('b'), Identity.of('c')),
-      Identity.of('abc')
+    assert.isTrue(
+      R.equals(
+        RA.liftFN(3, add3)(
+          Identity.of('a'),
+          Identity.of('b'),
+          Identity.of('c')
+        ),
+        Identity.of('abc')
+      )
     );
   });
 
   it('is curried', function() {
     const f4 = RA.liftFN(4);
     assert.strictEqual(typeof f4, 'function');
-    assert.deepEqual(
-      f4(addN)(Maybe.Some(1), Maybe.Some(1), Maybe.Some(1), Maybe.Some(1)),
-      Maybe.Some(4)
+    assert.isTrue(
+      R.equals(
+        f4(addN)(Maybe.Some(1), Maybe.Some(1), Maybe.Some(1), Maybe.Some(1)),
+        Maybe.Some(4)
+      )
     );
   });
 });
