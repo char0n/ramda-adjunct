d5a4bd129c32fcc764433ccbe400d2bc47c4ad03
js636f <js636f@gmail.com>
2019-12-06 11:34:21 +0200

    test(files): add ramda placeholder and make tests consitent, B0

--- test/isNotNull.js
+++ test/isNotNull.js
@@ -1,14 +1,28 @@
 import { assert } from 'chai';
+import * as R from 'ramda';
 
 import * as RA from '../src';
 
 describe('isNotNull', function() {
-  it('tests a value for complement of `null`', function() {
-    assert.isTrue(RA.isNotNull(void 0));
-    assert.isFalse(RA.isNotNull(null));
-    assert.isTrue(RA.isNotNull([]));
-    assert.isTrue(RA.isNotNull({}));
-    assert.isTrue(RA.isNotNull(0));
-    assert.isTrue(RA.isNotNull(''));
+  context('given non null value', function() {
+    specify('should return true', function() {
+      assert.isTrue(RA.isNotNull(void 0));
+      assert.isTrue(RA.isNotNull([]));
+      assert.isTrue(RA.isNotNull({}));
+      assert.isTrue(RA.isNotNull(0));
+      assert.isTrue(RA.isNotNull(''));
+    });
+  });
+
+  context('given null value', function() {
+    specify('should return false', function() {
+      assert.isFalse(RA.isNotNull(null));
+    });
+  });
+
+  it('should support placeholder to specify "gaps"', function() {
+    const isNotNull = RA.isNotNull(R.__);
+
+    assert.isTrue(isNotNull(0));
   });
 });
