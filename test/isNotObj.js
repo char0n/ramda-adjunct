ee6188ce4f763f9a46133e910534bded2872dd45
js636f <js636f@gmail.com>
2019-12-12 12:50:41 +0200

    test(isNotObj): add ramda placeholder and make tests consitent
    Ref #624

--- test/isNotObj.js
+++ test/isNotObj.js
@@ -1,29 +1,52 @@
 import { assert } from 'chai';
+import * as R from 'ramda';
 
 import * as RA from '../src';
 import args from './shared/arguments';
 import Symbol from './shared/Symbol';
 
 describe('isNotObj', function() {
-  it('tests a value for complement of language type of `Object`', function() {
-    assert.isFalse(RA.isNotObj({}));
-    assert.isFalse(RA.isNotObj(Object(false)));
-    assert.isFalse(RA.isNotObj(Object.create(null)));
-    assert.isFalse(RA.isNotObj(args));
-    assert.isFalse(RA.isNotObj([1, 2, 3]));
-    assert.isFalse(RA.isNotObj(Object(false)));
-    assert.isFalse(RA.isNotObj(new Date()));
-    assert.isFalse(RA.isNotObj(new Error()));
-    assert.isFalse(RA.isNotObj(RA));
-    assert.isFalse(RA.isNotObj(Array.prototype.slice));
-    assert.isFalse(RA.isNotObj({ a: 1 }));
-    assert.isFalse(RA.isNotObj(Object(0)));
-    assert.isFalse(RA.isNotObj(/x/));
-    assert.isFalse(RA.isNotObj(Object('a')));
-    assert.strictEqual(RA.isNotObj(Symbol), typeof Symbol === 'undefined');
+  context('given non object value', function() {
+    specify('should return true', function() {
+      assert.isTrue(RA.isNotObj(null));
+      assert.isTrue(RA.isNotObj(undefined));
+    });
+  });
 
-    assert.isTrue(RA.isNotObj(null));
-    assert.isTrue(RA.isNotObj(undefined));
+  context('given object value', function() {
+    specify('should return false', function() {
+      assert.isFalse(RA.isNotObj({}));
+      assert.isFalse(RA.isNotObj(Object(false)));
+      assert.isFalse(RA.isNotObj(Object.create(null)));
+      assert.isFalse(RA.isNotObj(args));
+      assert.isFalse(RA.isNotObj([1, 2, 3]));
+      assert.isFalse(RA.isNotObj(Object(false)));
+      assert.isFalse(RA.isNotObj(new Date()));
+      assert.isFalse(RA.isNotObj(new Error()));
+      assert.isFalse(RA.isNotObj(RA));
+      assert.isFalse(RA.isNotObj(Array.prototype.slice));
+      assert.isFalse(RA.isNotObj({ a: 1 }));
+      assert.isFalse(RA.isNotObj(Object(0)));
+      assert.isFalse(RA.isNotObj(/x/));
+      assert.isFalse(RA.isNotObj(Object('a')));
+
+      if (Symbol !== 'undefined') {
+        assert.isFalse(RA.isNotObj(Symbol));
+      }
+    });
+  });
+
+  /*
+   We have a bug here
+   RA.isNotObj(R.__) returns false. I think the problem is in
+   src/isObj.js because src/isNotObj.js is a complement(isObj)
+   and RA.isObj(R.__) just returns true, so I think we need to
+   refactor src/isObj.js to eliminate the bug.
+  */
+  it('should support placeholder to specify "gaps"', function() {
+    const isNotObj = RA.isNotObj(R.__);
+
+    assert.isTrue(isNotObj(null));
   });
 });
 
