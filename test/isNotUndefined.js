e67a01de927cb44eea908294f709be9587010314
js636f <js636f@gmail.com>
2019-12-26 12:49:58 +0200

    test: add ramda placeholder and make tests consistent
    Ref #624

--- test/isNotUndefined.js
+++ test/isNotUndefined.js
@@ -1,15 +1,34 @@
 import { assert } from 'chai';
+import * as R from 'ramda';
 
 import * as RA from '../src';
+import Symbol from './shared/Symbol';
 
 describe('isNotUndefined', function() {
-  it('tests a value for complement of `undefined`', function() {
-    assert.isFalse(RA.isNotUndefined(void 0));
-    assert.isFalse(RA.isNotUndefined(undefined));
-    assert.isTrue(RA.isNotUndefined(null));
-    assert.isTrue(RA.isNotUndefined([]));
-    assert.isTrue(RA.isNotUndefined({}));
-    assert.isTrue(RA.isNotUndefined(0));
-    assert.isTrue(RA.isNotUndefined(''));
+  context('given a non-undefined value', function() {
+    specify('it should return true', function() {
+      assert.isTrue(RA.isNotUndefined(null));
+      assert.isTrue(RA.isNotUndefined([]));
+      assert.isTrue(RA.isNotUndefined({}));
+      assert.isTrue(RA.isNotUndefined(0));
+      assert.isTrue(RA.isNotUndefined(''));
+
+      if (Symbol !== 'undefined') {
+        assert.isTrue(RA.isNotUndefined(Symbol));
+      }
+    });
+  });
+
+  context('given an undefined value', function() {
+    specify('it should return false', function() {
+      assert.isFalse(RA.isNotUndefined(void 0));
+      assert.isFalse(RA.isNotUndefined(undefined));
+    });
+  });
+
+  it('should support placeholder to specify "gaps"', function() {
+    const isNotUndefined = RA.isNotUndefined(R.__);
+
+    assert.isTrue(isNotUndefined(null));
   });
 });
--- test/isNotValidDate.js
+++ test/isNotValidDate.js
@@ -1,37 +1,50 @@
 import { assert } from 'chai';
+import * as R from 'ramda';
 
 import * as RA from '../src';
 import args from './shared/arguments';
 import Symbol from './shared/Symbol';
 
 describe('isNotValidDate', function() {
-  it('tests a value for complement of valid `Date`', function() {
-    assert.isFalse(RA.isNotValidDate(new Date()));
+  context('given a non-date value', function() {
+    specify('it should return true', function() {
+      assert.isTrue(RA.isNotValidDate(new Date('a')));
+      assert.isTrue(RA.isNotValidDate(Date.now()));
+      assert.isTrue(RA.isNotValidDate(args));
+      assert.isTrue(RA.isNotValidDate([1, 2, 3]));
+      assert.isTrue(RA.isNotValidDate(Object(true)));
+      assert.isTrue(RA.isNotValidDate(new Error()));
+      assert.isTrue(RA.isNotValidDate(RA));
+      assert.isTrue(RA.isNotValidDate(Array.prototype.slice));
+      assert.isTrue(RA.isNotValidDate({ a: 1 }));
+      assert.isTrue(RA.isNotValidDate(Object(0)));
+      assert.isTrue(RA.isNotValidDate(/x/));
+      assert.isTrue(RA.isNotValidDate(Object('a')));
 
-    assert.isTrue(RA.isNotValidDate(new Date('a')));
-    assert.isTrue(RA.isNotValidDate(Date.now()));
-    assert.isTrue(RA.isNotValidDate(args));
-    assert.isTrue(RA.isNotValidDate([1, 2, 3]));
-    assert.isTrue(RA.isNotValidDate(Object(true)));
-    assert.isTrue(RA.isNotValidDate(new Error()));
-    assert.isTrue(RA.isNotValidDate(RA));
-    assert.isTrue(RA.isNotValidDate(Array.prototype.slice));
-    assert.isTrue(RA.isNotValidDate({ a: 1 }));
-    assert.isTrue(RA.isNotValidDate(Object(0)));
-    assert.isTrue(RA.isNotValidDate(/x/));
-    assert.isTrue(RA.isNotValidDate(Object('a')));
+      if (Symbol !== 'undefined') {
+        assert.isTrue(RA.isNotValidDate(Symbol));
+      }
 
-    if (Symbol !== 'undefined') {
-      assert.isTrue(RA.isNotValidDate(Symbol));
-    }
+      assert.isTrue(RA.isNotValidDate(null));
+      assert.isTrue(RA.isNotValidDate(undefined));
+    });
+  });
 
-    assert.isTrue(RA.isNotValidDate(null));
-    assert.isTrue(RA.isNotValidDate(undefined));
+  context('given a date value', function() {
+    specify('it should return false', function() {
+      assert.isFalse(RA.isNotValidDate(new Date()));
+    });
+  });
+
+  it('should support placeholder to specify "gaps"', function() {
+    const isNotValidDate = RA.isNotValidDate(R.__);
+
+    assert.isTrue(isNotValidDate(null));
   });
 });
 
 describe('isInvalidDate', function() {
-  it('tests an alias', function() {
+  it('should be an alias for isNotValidDate', function() {
     assert.strictEqual(RA.isNotValidDate, RA.isInvalidDate);
   });
 });
