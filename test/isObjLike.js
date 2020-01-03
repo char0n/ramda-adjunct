34e24352b05880ec99233aa477f640788ac0cbe1
Alex <js636f@gmail.com>
2020-01-03 15:09:45 +0200

    test: add ramda placeholder and make tests consistent (#1251)
    Ref #624

--- test/isObjLike.js
+++ test/isObjLike.js
@@ -1,36 +1,49 @@
 import { assert } from 'chai';
+import * as R from 'ramda';
 
 import * as RA from '../src';
 import args from './shared/arguments';
 import Symbol from './shared/Symbol';
 
 describe('isObjLike', function() {
-  it('tests a value for object-like interface', function() {
-    assert.isTrue(RA.isObjLike({}));
-    assert.isTrue(RA.isObjLike(Object(true)));
-    assert.isTrue(RA.isObjLike(args));
-    assert.isTrue(RA.isObjLike([1, 2, 3]));
-    assert.isTrue(RA.isObjLike(Object(false)));
-    assert.isTrue(RA.isObjLike(new Date()));
-    assert.isTrue(RA.isObjLike(new Error()));
-    assert.isTrue(RA.isObjLike(RA));
-    assert.isTrue(RA.isObjLike({ a: 1 }));
-    assert.isTrue(RA.isObjLike(Object(0)));
-    assert.isTrue(RA.isObjLike(/x/));
-    assert.isTrue(RA.isObjLike(Object('a')));
+  context('given a value with object-like interface', function() {
+    specify('should return true', function() {
+      assert.isTrue(RA.isObjLike({}));
+      assert.isTrue(RA.isObjLike(Object(true)));
+      assert.isTrue(RA.isObjLike(args));
+      assert.isTrue(RA.isObjLike([1, 2, 3]));
+      assert.isTrue(RA.isObjLike(Object(false)));
+      assert.isTrue(RA.isObjLike(new Date()));
+      assert.isTrue(RA.isObjLike(new Error()));
+      assert.isTrue(RA.isObjLike(RA));
+      assert.isTrue(RA.isObjLike({ a: 1 }));
+      assert.isTrue(RA.isObjLike(Object(0)));
+      assert.isTrue(RA.isObjLike(/x/));
+      assert.isTrue(RA.isObjLike(Object('a')));
+    });
+  });
 
-    if (Symbol !== 'undefined') {
-      assert.isFalse(RA.isObjLike(Symbol));
-    }
+  context('given a value with non object-like interface', function() {
+    specify('should return false', function() {
+      if (Symbol !== 'undefined') {
+        assert.isFalse(RA.isObjLike(Symbol));
+      }
 
-    assert.isFalse(RA.isObjLike(Array.prototype.slice));
-    assert.isFalse(RA.isObjLike(null));
-    assert.isFalse(RA.isObjLike(undefined));
+      assert.isFalse(RA.isObjLike(Array.prototype.slice));
+      assert.isFalse(RA.isObjLike(null));
+      assert.isFalse(RA.isObjLike(undefined));
+    });
+  });
+
+  it('should support placeholder to specify "gaps"', function() {
+    const isObjLike = RA.isObjLike(R.__);
+
+    assert.isTrue(isObjLike({}));
   });
 });
 
 describe('isObjectLike', function() {
-  it('tests an alias', function() {
+  it('should be an alias for isObjLike', function() {
     assert.strictEqual(RA.isObjLike, RA.isObjectLike);
   });
 });
