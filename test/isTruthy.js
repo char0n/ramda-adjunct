07ffe1e1343aec67c4312d6985c237aeebe679f0
js636f <js636f@gmail.com>
2020-01-09 13:23:17 +0200

    test: add ramda placeholder and make tests consistent
    Ref #624

--- test/isTruthy.js
+++ test/isTruthy.js
@@ -1,37 +1,50 @@
 import { assert } from 'chai';
+import * as R from 'ramda';
 
 import * as RA from '../src';
 import Symbol from './shared/Symbol';
 import args from './shared/arguments';
 
 describe('isTruthy', function() {
-  it('tests a value for `truthy`', function() {
-    assert.isTrue(RA.isTruthy('abc'));
-    assert.isTrue(RA.isTruthy(Object('abc')));
-    assert.isTrue(RA.isTruthy(args));
-    assert.isTrue(RA.isTruthy([1, 2, 3]));
-    assert.isTrue(RA.isTruthy(true));
-    assert.isTrue(RA.isTruthy(new Date()));
-    assert.isTrue(RA.isTruthy(new Error()));
-    assert.isTrue(RA.isTruthy(Array.prototype.slice));
-    assert.isTrue(RA.isTruthy({ 0: 1, length: 1 }));
-    assert.isTrue(RA.isTruthy(1));
-    assert.isTrue(RA.isTruthy(/x/));
+  context('given a truthy value', function() {
+    specify('should return true', function() {
+      assert.isTrue(RA.isTruthy('abc'));
+      assert.isTrue(RA.isTruthy(Object('abc')));
+      assert.isTrue(RA.isTruthy(args));
+      assert.isTrue(RA.isTruthy([1, 2, 3]));
+      assert.isTrue(RA.isTruthy(true));
+      assert.isTrue(RA.isTruthy(new Date()));
+      assert.isTrue(RA.isTruthy(new Error()));
+      assert.isTrue(RA.isTruthy(Array.prototype.slice));
+      assert.isTrue(RA.isTruthy({ 0: 1, length: 1 }));
+      assert.isTrue(RA.isTruthy(1));
+      assert.isTrue(RA.isTruthy(/x/));
 
-    if (Symbol !== 'undefined') {
-      assert.isTrue(RA.isTruthy(Symbol));
-    }
+      if (Symbol !== 'undefined') {
+        assert.isTrue(RA.isTruthy(Symbol));
+      }
 
-    assert.isTrue(RA.isTruthy({}));
-    assert.isTrue(RA.isTruthy([]));
-    assert.isTrue(RA.isTruthy(Infinity));
+      assert.isTrue(RA.isTruthy({}));
+      assert.isTrue(RA.isTruthy([]));
+      assert.isTrue(RA.isTruthy(Infinity));
+    });
+  });
 
-    assert.isFalse(RA.isTruthy(false));
-    assert.isFalse(RA.isTruthy(0));
-    assert.isFalse(RA.isTruthy(-0));
-    assert.isFalse(RA.isTruthy(''));
-    assert.isFalse(RA.isTruthy(null));
-    assert.isFalse(RA.isTruthy(undefined));
-    assert.isFalse(RA.isTruthy(NaN));
+  context('given a falsy value', function() {
+    specify('should return false', function() {
+      assert.isFalse(RA.isTruthy(false));
+      assert.isFalse(RA.isTruthy(0));
+      assert.isFalse(RA.isTruthy(-0));
+      assert.isFalse(RA.isTruthy(''));
+      assert.isFalse(RA.isTruthy(null));
+      assert.isFalse(RA.isTruthy(undefined));
+      assert.isFalse(RA.isTruthy(NaN));
+    });
+  });
+
+  it('should support placeholder to specify "gaps"', function() {
+    const isTruthy = RA.isTruthy(R.__);
+
+    assert.isTrue(isTruthy(true));
   });
 });
--- test/isValidDate.js
+++ test/isValidDate.js
@@ -1,31 +1,44 @@
 import { assert } from 'chai';
+import * as R from 'ramda';
 
 import * as RA from '../src';
 import args from './shared/arguments';
 import Symbol from './shared/Symbol';
 
 describe('isValidDate', function() {
-  it('tests a value for a valid `Date`', function() {
-    assert.isTrue(RA.isValidDate(new Date()));
+  context('given a valid `Date`', function() {
+    specify('should return true', function() {
+      assert.isTrue(RA.isValidDate(new Date()));
+    });
+  });
 
-    assert.isFalse(RA.isValidDate(new Date('a')));
-    assert.isFalse(RA.isValidDate(Date.now()));
-    assert.isFalse(RA.isValidDate(args));
-    assert.isFalse(RA.isValidDate([1, 2, 3]));
-    assert.isFalse(RA.isValidDate(Object(false)));
-    assert.isFalse(RA.isValidDate(new Error()));
-    assert.isFalse(RA.isValidDate(RA));
-    assert.isFalse(RA.isValidDate(Array.prototype.slice));
-    assert.isFalse(RA.isValidDate({ a: 1 }));
-    assert.isFalse(RA.isValidDate(Object(0)));
-    assert.isFalse(RA.isValidDate(/x/));
-    assert.isFalse(RA.isValidDate(Object('a')));
+  context('given an invalid `Date` or non `Date`', function() {
+    specify('should return false', function() {
+      assert.isFalse(RA.isValidDate(new Date('a')));
+      assert.isFalse(RA.isValidDate(Date.now()));
+      assert.isFalse(RA.isValidDate(args));
+      assert.isFalse(RA.isValidDate([1, 2, 3]));
+      assert.isFalse(RA.isValidDate(Object(false)));
+      assert.isFalse(RA.isValidDate(new Error()));
+      assert.isFalse(RA.isValidDate(RA));
+      assert.isFalse(RA.isValidDate(Array.prototype.slice));
+      assert.isFalse(RA.isValidDate({ a: 1 }));
+      assert.isFalse(RA.isValidDate(Object(0)));
+      assert.isFalse(RA.isValidDate(/x/));
+      assert.isFalse(RA.isValidDate(Object('a')));
 
-    if (Symbol !== 'undefined') {
-      assert.isFalse(RA.isValidDate(Symbol));
-    }
+      if (Symbol !== 'undefined') {
+        assert.isFalse(RA.isValidDate(Symbol));
+      }
 
-    assert.isFalse(RA.isValidDate(null));
-    assert.isFalse(RA.isValidDate(undefined));
+      assert.isFalse(RA.isValidDate(null));
+      assert.isFalse(RA.isValidDate(undefined));
+    });
+  });
+
+  it('should support placeholder to specify "gaps"', function() {
+    const isValidDate = RA.isValidDate(R.__);
+
+    assert.isTrue(isValidDate(new Date()));
   });
 });
