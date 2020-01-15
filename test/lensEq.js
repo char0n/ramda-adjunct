457b3b7c486a3ffd61929f10f219d7d3a7dae50f
js636f <js636f@gmail.com>
2020-01-15 12:34:10 +0200

    test: add ramda placeholder and make tests consistent
    Ref #624

--- test/lensEq.js
+++ test/lensEq.js
@@ -4,18 +4,35 @@ import { lens, lensIndex, lensPath, lensProp, path } from 'ramda';
 import * as RA from '../src';
 
 describe('lensEq', function() {
-  it('tests a lens for a value', function() {
-    assert.isTrue(RA.lensEq(lensIndex(1), 1, [0, 1]));
-    assert.isFalse(RA.lensEq(lensIndex(0), 1, [0, 1]));
-    assert.isTrue(RA.lensEq(lensPath(['a', 'b']), 'foo', { a: { b: 'foo' } }));
-    assert.isFalse(RA.lensEq(lensPath(['b', 'a']), 'foo', { a: { b: 'foo' } }));
-    assert.isTrue(RA.lensEq(lens(path(['a']), RA.noop), 'b', { a: 'b' }));
-    assert.isFalse(RA.lensEq(lens(path(['a']), RA.noop), 'b', { a: 'c' }));
-    assert.isTrue(RA.lensEq(lensProp('foo'), 'bar', { foo: 'bar' }));
-    assert.isFalse(RA.lensEq(lensProp('foo'), 'bar', { bar: 'foo' }));
-  });
+  context(
+    'given a data structure focused by the given lens equals provided value',
+    function() {
+      specify('should return true', function() {
+        assert.isTrue(RA.lensEq(lensIndex(1), 1, [0, 1]));
+        assert.isTrue(
+          RA.lensEq(lensPath(['a', 'b']), 'foo', { a: { b: 'foo' } })
+        );
+        assert.isTrue(RA.lensEq(lens(path(['a']), RA.noop), 'b', { a: 'b' }));
+        assert.isTrue(RA.lensEq(lensProp('foo'), 'bar', { foo: 'bar' }));
+      });
+    }
+  );
 
-  it('tests currying', function() {
+  context(
+    'given a data structure focused by the given lens non equals provided value',
+    function() {
+      specify('should return false', function() {
+        assert.isFalse(RA.lensEq(lensIndex(0), 1, [0, 1]));
+        assert.isFalse(
+          RA.lensEq(lensPath(['b', 'a']), 'foo', { a: { b: 'foo' } })
+        );
+        assert.isFalse(RA.lensEq(lens(path(['a']), RA.noop), 'b', { a: 'c' }));
+        assert.isFalse(RA.lensEq(lensProp('foo'), 'bar', { bar: 'foo' }));
+      });
+    }
+  );
+
+  it('should be curried', function() {
     assert.isTrue(RA.lensEq(lensIndex(1))(1)([0, 1]));
     assert.isTrue(RA.lensEq(lensIndex(1), 1)([0, 1]));
     assert.isTrue(RA.lensEq(lensIndex(1))(1, [0, 1]));
