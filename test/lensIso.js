3a5c972f4a030914924b0985e071e982bca637a5
js636f <js636f@gmail.com>
2020-01-16 11:25:43 +0200

    test: add ramda placeholder and make tests consistent
    Ref #624

--- test/lensIso.js
+++ test/lensIso.js
@@ -6,47 +6,35 @@ import * as RA from '../src';
 describe('lensIso', function() {
   const lensJSON = RA.lensIso(JSON.parse, JSON.stringify);
 
-  it('tests currying with arity 1', function() {
-    const lensJSONA1 = RA.lensIso(JSON.parse)(JSON.stringify);
+  context('given isomorphism', function() {
+    specify('should read through lens', function() {
+      assert.deepEqual(view(lensJSON, '{"a":1}'), { a: 1 });
+    });
 
-    assert.deepEqual(view(lensJSONA1, '{"a":1}'), { a: 1 });
-  });
+    specify('should write through lens', function() {
+      assert.strictEqual(set(lensJSON, { b: 2 }, '{"a": 1}'), '{"b":2}');
+    });
 
-  it('tests currying with arity 2', function() {
-    assert.deepEqual(view(lensJSON, '{"a":1}'), { a: 1 });
-  });
-
-  it('tests for `from` property', function() {
-    assert.isTrue(RA.isFunction(RA.lensIso.from));
-  });
-
-  it('tests reading through lens', function() {
-    assert.deepEqual(view(lensJSON, '{"a":1}'), { a: 1 });
-  });
-
-  it('tests writing through lens', function() {
-    assert.strictEqual(set(lensJSON, { b: 2 }, '{"a": 1}'), '{"b":2}');
-  });
-
-  it('tests applying a function over lens', function() {
-    assert.strictEqual(
-      over(lensJSON, assoc('b', 2), '{"a":1}'),
-      '{"a":1,"b":2}'
-    );
+    specify('should apply a function over lens', function() {
+      assert.strictEqual(
+        over(lensJSON, assoc('b', 2), '{"a":1}'),
+        '{"a":1,"b":2}'
+      );
+    });
   });
 
   context('given reversed isomorphism', function() {
     const lensJSONReversed = RA.lensIso.from(lensJSON);
 
-    specify('test reading through lens', function() {
+    specify('should read through lens', function() {
       assert.strictEqual(view(lensJSONReversed, { a: 1 }), '{"a":1}');
     });
 
-    specify('tests writing through lens', function() {
+    specify('should write through lens', function() {
       assert.deepEqual(set(lensJSONReversed, '{"b":2}', { a: 1 }), { b: 2 });
     });
 
-    specify('tests applying a function over lens', function() {
+    specify('should apply a function over lens', function() {
       assert.deepEqual(
         over(lensJSONReversed, replace('}', ',"b":2}'), { a: 1 }),
         {
@@ -55,5 +43,16 @@ describe('lensIso', function() {
         }
       );
     });
+  });
+
+  it('should contain `from` property', function() {
+    assert.isTrue(RA.isFunction(RA.lensIso.from));
+  });
+
+  it('should be curried', function() {
+    const lensJSONA1 = RA.lensIso(JSON.parse)(JSON.stringify);
+
+    assert.deepEqual(view(lensJSONA1, '{"a":1}'), { a: 1 });
+    assert.deepEqual(view(lensJSON, '{"a":1}'), { a: 1 });
   });
 });
