757cd337a5c8c0378927ee6b8d0fe97bff1fc40f
siddhant <siddhant@systeminsights.com>
2020-02-22 12:42:27 +0530

    feat(function): fNull
    fnull is in a function that takes a function as an argument and a number of additional arguments, and returns a function that just calls the original function given. The magic of fnull is that if any of the arguments to the function that it returns are null or undefined, then the original “default” argument is used instead
    
    Closes #1235

---
+++ test/fNull.js
@@ -0,0 +1,79 @@
+import { assert } from 'chai';
+
+import * as RA from '../src';
+
+describe('fNull', function() {
+  context('given function but no default arguments', function() {
+    specify('should call the function with the given arguments', function() {
+      const add = (a, b) => a + b;
+
+      const safeAdd = RA.fNull(add, []);
+
+      const value = safeAdd(1, 2);
+
+      assert.equal(value, 3);
+    });
+
+    specify(
+      'should call the function with the given argument and undefined',
+      function() {
+        const add = (a, b) => a + b;
+
+        const safeAdd = RA.fNull(add, []);
+
+        const value = safeAdd(1, null);
+
+        assert.isNaN(value);
+      }
+    );
+  });
+
+  context('given function and default arguments', function() {
+    specify(
+      'should call the function with the given argument and null default argument',
+      function() {
+        const add = (a, b, c) => a + b + c;
+
+        const safeAdd = RA.fNull(add, null);
+
+        const value = safeAdd(1, 1, null);
+
+        assert.equal(value, 2);
+      }
+    );
+    specify(
+      'should call the function with the given argument and default argument',
+      function() {
+        const add = (a, b) => a + b;
+
+        const safeAdd = RA.fNull(add, [1, 1]);
+
+        const value = safeAdd(1, null);
+
+        assert.equal(value, 2);
+      }
+    );
+  });
+
+  context('given argument which is not a function', function() {
+    specify('should return undefined', function() {
+      const safeAdd = RA.fNull(null, [1, 1]);
+
+      const value = safeAdd(1);
+
+      assert.isUndefined(value);
+    });
+  });
+
+  it('should curry', async function() {
+    const add = (a, b) => a + b;
+
+    const safeAdd = RA.fNull(add);
+
+    const safeAddWithDefaults = safeAdd([1, 1]);
+
+    const value = safeAddWithDefaults(1, null);
+
+    assert.equal(value, 2);
+  });
+});
