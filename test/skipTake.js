867f1fca74458902c0814a6328fe606f0d676b9e
Damian Green <damian@gcoders.com>
2020-03-10 23:58:30 +0000

    should support placeholder to specify "gaps

--- test/skipTake.js
+++ test/skipTake.js
@@ -1,5 +1,6 @@
 import { assert } from 'chai';
 import { range } from 'ramda';
+import * as R from 'ramda';
 
 import * as RA from '../src';
 
@@ -10,5 +11,10 @@ describe('skipTake', function() {
       assert.deepEqual(RA.skipTake(2)([1, 2, 3, 4]), [1, 3]);
       assert.deepEqual(RA.skipTake(3, range(0, 20)), [0, 3, 6, 9, 12, 15, 18]);
     });
+  });
+
+  it('should support placeholder to specify "gaps"', function() {
+    const skipTake = RA.skipTake(R.__);
+    assert.deepEqual(skipTake(2, [1, 2, 3, 4]), [1, 3]);
   });
 });
