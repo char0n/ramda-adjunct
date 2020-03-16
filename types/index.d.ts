10c41411733f5d07f8450ad4da59d11d395ae221
Damian Green <damian@gcoders.com>
2020-03-16 15:56:59 +0000

    feat: add skipTake (#1338)
    Closes #1237

--- types/index.d.ts
+++ types/index.d.ts
@@ -1352,6 +1352,12 @@ declare namespace RamdaAdjunct {
         sortByProps(props: string[]): (list: object[]) => object[];
 
         /**
+         * When given a number n and an array, returns an array containing every nth element.
+         */
+        skipTake<T>(n: number, list: T[]): T[];
+        skipTake<T>(n: number): (list: T[]) => T[];
+
+        /**
          * Determine if input value is an indexed data type.
          */
         isIndexed(val: any): val is string | any[];
