04c95fd63b45388e8849f3a94b29e98e23afed98
Damian Green <damian@gcoders.com>
2020-03-16 11:30:53 +0000

    feat: skipTake

--- test/skipTake.js
+++ test/skipTake.js
@@ -22,6 +22,12 @@ describe('skipTake', function() {
     });
   });
 
+  context('given negative count as input and a non empty array', function() {
+    specify('should ignore the sign of count', function() {
+      assert.deepEqual(RA.skipTake(-2, [1, 2, 3, 4]), [1, 3]);
+    });
+  });
+
   context('given first argument is 0', function() {
     specify('should return an empty array', function() {
       assert.deepEqual(RA.skipTake(0, []), []);
