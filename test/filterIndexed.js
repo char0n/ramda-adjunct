8bedfbf000cbe48aff5769cae10e5b9b4d97c324
William Blake <gwilliamblake@gmail.com>
2020-03-18 11:58:35 -0500

    feat(src): add filterIndexed
    expose index to the predicate function
    
    Ref #1346

---
+++ test/filterIndexed.js
@@ -0,0 +1,14 @@
+import { assert } from 'chai';
+
+import * as RA from '../src';
+
+describe('filterIndexed', function() {
+  context('given documentation example', function() {
+    specify('should return an array of the value at a given index', function() {
+      assert.deepEqual(
+        RA.filterIndexed((v, idx) => idx === 0, [1, 2, 3]),
+        [1]
+      );
+    });
+  });
+});
