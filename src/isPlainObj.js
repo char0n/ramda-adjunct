4de1ff096fe9e6c942b30a4903d4ca51d31c88d0
William Blake <gwilliamblake@gmail.com>
2020-03-18 19:33:47 -0500

    refactor(src): remove internal ramda reference
    replace internal ramda _isObject
    
    Ref #1361

--- src/isPlainObj.js
+++ src/isPlainObj.js
@@ -1,10 +1,11 @@
-import _isObject from 'ramda/src/internal/_isObject';
 import { pipe, both, equals, toString, pathSatisfies, curryN } from 'ramda';
 
 import isNull from './isNull';
 import isObjLike from './isObjLike';
 import isFunction from './isFunction';
 
+const isObject = val =>
+  Object.prototype.toString.call(val) === '[object Object]';
 const isObjectConstructor = pipe(toString, equals(toString(Object)));
 const hasObjectConstructor = pathSatisfies(
   both(isFunction, isObjectConstructor),
@@ -40,7 +41,7 @@ const hasObjectConstructor = pathSatisfies(
  */
 /* eslint-enable max-len */
 const isPlainObj = curryN(1, val => {
-  if (!isObjLike(val) || !_isObject(val)) {
+  if (!isObjLike(val) || !isObject(val)) {
     return false;
   }
 
