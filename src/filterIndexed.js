e4306bdd706bb7dbe9c3b4b6075cf54356ed275b
William Blake <gwilliamblake@gmail.com>
2020-03-19 19:58:09 -0500

    Apply suggestions from code review
    Co-Authored-By: Vladimir Gorej <vladimir.gorej@gmail.com>

--- src/filterIndexed.js
+++ src/filterIndexed.js
@@ -1,19 +1,28 @@
 import { addIndex, filter } from 'ramda';
 
 /**
- * const filterIndexed = R.addIndex(R.filter);
+ * {@link http://ramdajs.com/docs/#filter|R.filter} function that more closely resembles Array.prototype.filter.
+ * It takes two new parameters to its callback function: the current index, and the entire list.
+ * 
+ * `filterIndexed` implementation is simple : `
+ *  const filterIndexed = R.addIndex(R.filter);
+ * `
  *
  * @func filterIndexed
- *
+ * @memberOf RA
+ * @since {@link https://char0n.github.io/ramda-adjunct/2.27.0|v2.27.0}
+ * @category List
  * @typedef Idx = Number
+ * @sig Filterable f => ((a, Idx, f a) -> Boolean) -> f a -> f a
  * @param {Function} fn The predicate function
- * @param {Array} list the list to filter
- * @param {*} val
- * @return {Array} A filterable containing the value at a given index
- * @see {@link http: //ramdajs.com/docs/#addIndex|R.addIndex}, {@link https://ramdajs.com/docs/#filter}
+ * @param {Array} list The list to filter
+ * @return {Array} Filterable
+ * @see {@link http://ramdajs.com/docs/#addIndex|R.addIndex}, {@link http://ramdajs.com/docs/#filter|R.filter}
  * @example
  *
- * filterIndexed((val, idx) => idx === 0, [1, 2, 3]); // => [1]
+const isEven = n => n % 2 === 0;
+
+R.filterIndexed((val, idx) => isEven(idx), [1, 2, 3, 4]); //=> [1, 3]
  */
 
 const filterIndexed = addIndex(filter);
