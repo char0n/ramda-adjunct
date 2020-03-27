2e21ada53731e23d5462db3b9b0110af02fe6b55
Alex <js636f@gmail.com>
2020-03-27 11:17:08 +0200

    test: add ramda placeholder and make tests consistent
    Ref #624

--- test/weave.js
+++ test/weave.js
@@ -11,7 +11,7 @@ describe('weave', function () {
   const mixedReader = (a, b, ...args) =>
     reader((config) => sum(args.concat(a, b, config)));
 
-  it('tests weaving', function () {
+  it('should weave a configuration into function', function () {
     const wunaryReader = RA.weave(unaryReader, 1);
     const wbinaryReader = RA.weave(binaryReader, 1);
 
@@ -19,7 +19,7 @@ describe('weave', function () {
     assert.strictEqual(wbinaryReader(2, 3), 6);
   });
 
-  it('tests currying', function () {
+  it('should be curried', function () {
     const wbinaryReader1 = RA.weave(binaryReader, 1);
     const wbinaryReader2 = RA.weave(binaryReader)(1);
 
@@ -27,43 +27,51 @@ describe('weave', function () {
     assert.strictEqual(wbinaryReader2(2, 3), 6);
   });
 
-  it('tests auto-currying on fixed function signature', function () {
+  it('should support auto-currying on fixed function signature', function () {
     const wbinaryReader = RA.weave(binaryReader, 1);
 
     assert.strictEqual(wbinaryReader(2, 3), 6);
     assert.strictEqual(wbinaryReader(2)(3), 6);
   });
 
-  it('tests auto-currying on curried fixed function signature', function () {
-    const wbinaryReader = RA.weave(curry(binaryReader), 1);
+  it('should support auto-currying on curried fixed function signature',
+    function () {
+      const wbinaryReader = RA.weave(curry(binaryReader), 1);
 
-    assert.strictEqual(wbinaryReader(2, 3), 6);
-    assert.strictEqual(wbinaryReader(2)(3), 6);
-  });
+      assert.strictEqual(wbinaryReader(2, 3), 6);
+      assert.strictEqual(wbinaryReader(2)(3), 6);
+    }
+  );
 
-  it('tests auto-currying on variadic function signature', function () {
-    const wvariadicReader = RA.weave(variadicReader, 1);
+  it('should support auto-currying on variadic function signature',
+    function () {
+      const wvariadicReader = RA.weave(variadicReader, 1);
 
-    assert.strictEqual(wvariadicReader(1, 2, 3), 7);
-  });
+      assert.strictEqual(wvariadicReader(1, 2, 3), 7);
+    }
+  );
 
-  it('tests auto-currying on curried variadic function signature', function () {
-    const wvariadicReader = RA.weave(curry(variadicReader), 1);
+  it('should support auto-currying on curried variadic function signature',
+    function () {
+      const wvariadicReader = RA.weave(curry(variadicReader), 1);
 
-    assert.strictEqual(wvariadicReader(1, 2, 3), 7);
-  });
+      assert.strictEqual(wvariadicReader(1, 2, 3), 7);
+    }
+  );
 
-  it('tests auto-currying on mixed function signature', function () {
+  it('should support auto-currying on mixed function signature', function () {
     const wmixedReader = RA.weave(mixedReader, 1);
 
     assert.strictEqual(wmixedReader(2, 3, 4), 10);
     assert.strictEqual(wmixedReader(2)(3, 4), 10);
   });
 
-  it('tests auto-currying on curried mixed function signature', function () {
-    const wmixedReader = RA.weave(curry(mixedReader), 1);
+  it('should support auto-currying on curried mixed function signature',
+    function () {
+      const wmixedReader = RA.weave(curry(mixedReader), 1);
 
-    assert.strictEqual(wmixedReader(2, 3, 4), 10);
-    assert.strictEqual(wmixedReader(2)(3, 4), 10);
-  });
+      assert.strictEqual(wmixedReader(2, 3, 4), 10);
+      assert.strictEqual(wmixedReader(2)(3, 4), 10);
+    }
+  );
 });
