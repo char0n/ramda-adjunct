f86ed27a1473a99a078aebea766bc0eaea9b6d51
Felipe Parreira <felipeparreirafb@gmail.com>
2020-04-15 13:05:19 -0300

    feat: add isBigInt predicate function
    Closes #1253

---
+++ src/isBigInt.js
@@ -0,0 +1,23 @@
+import { curryN } from 'ramda';
+
+/**
+ * Checks if value is a Big Integer.
+ *
+ * @func isBigInt
+ * @memberOf RA
+ * @category Type
+ * @sig * -> Boolean
+ * @param {*} val The value to test
+ * @return {boolean}
+ * @example
+ *
+ * RA.isBigInt(5); // => false
+ * RA.isBigInt(Number.MAX_VALUE); // => false
+ * RA.isBigInt(-Infinity); // => false
+ * RA.isBigInt(10); // => false
+ * RA.isBigInt(10n); // => true
+ * RA.isBigInt(BitInt(9007199254740991)); // => true
+ */
+const isBigInt = curryN(1, (val) => typeof val === 'bigint');
+
+export default isBigInt;
--- types/index.d.ts
+++ types/index.d.ts
@@ -326,6 +326,11 @@ declare namespace RamdaAdjunct {
         isNotInteger(val: any): boolean;
 
         /**
+         * Checks whether the passed value is a `bigint`.
+         */
+        isBigInt(val: any): boolean;
+
+        /**
          * Checks whether the passed value is a `float`.
          */
         isFloat(val: any): val is number;
