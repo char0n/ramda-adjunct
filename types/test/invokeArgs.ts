f50faf0c360315edea2643aad859748a226aff9e
Alex <js636f@gmail.com>
2020-04-20 15:17:16 +0300

    Editing

--- types/test/invokeArgs.ts
+++ types/test/invokeArgs.ts
@@ -2,6 +2,6 @@ import * as RA from 'ramda-adjunct';
 
 RA.invokeArgs(['abs'], [-1], Math); // $ExpectType any
 RA.invokeArgs(['nonexistentMethod'], [-1], Math); // $ExpectType any
-RA.invokeArgs([0, 'a'], [-1], Math); // $ExpectType any
+RA.invokeArgs([0, 'a'], [-1], Math); // $ExpectError
 RA.invokeArgs([{}], [-1], Math); // $ExpectError
 RA.invokeArgs([0], {}, Math); // $ExpectError
