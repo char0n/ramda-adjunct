2d61936ff784aceaab8cd60cc7ef9f5042faa866
Felipe Parreira <felipeparreirafb@gmail.com>
2020-04-24 11:44:28 -0300

    test(isBigInt): add type tests

---
+++ types/test/isBigInt.ts
@@ -0,0 +1,16 @@
+import * as RA from 'ramda-adjunct';
+
+RA.isBigInt(3); // $ExpectType boolean
+RA.isBigInt(Math.pow(2, 53)); // $ExpectType boolean
+RA.isBigInt(Math.pow(2, 53) - 1); // $ExpectType boolean
+RA.isBigInt(NaN); // $ExpectType boolean
+RA.isBigInt(Infinity); // $ExpectType boolean
+RA.isBigInt('3'); // $ExpectType boolean
+RA.isBigInt(3.1); // $ExpectType boolean
+RA.isBigInt(3.0); // $ExpectType boolean
+RA.isBigInt('string'); // $ExpectType boolean
+RA.isBigInt(null); // $ExpectType boolean
+RA.isBigInt(undefined); // $ExpectType boolean
+RA.isBigInt({}); // $ExpectType boolean
+RA.isBigInt(() => { }); // $ExpectType boolean
+RA.isBigInt(true); // $ExpectType boolean
