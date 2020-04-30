7f79991a9cf959dfaf1d56906ab1d44d914b64e9
Felipe Parreira <felipeparreirafb@gmail.com>
2020-04-30 20:53:15 -0300

    test(isBigInt): test method for negative cases outside conditional

--- test/isBigInt.js
+++ test/isBigInt.js
@@ -30,50 +30,50 @@ describe('isBigInt', function () {
         );
       });
     });
-
-    context('given a non-big-integer value', function () {
-      specify('should return false', function () {
-        assert.isFalse(RA.isBigInt(new Date()));
-        assert.isFalse(RA.isBigInt(args));
-        assert.isFalse(RA.isBigInt([1, 2, 3]));
-        assert.isFalse(RA.isBigInt(Object(false)));
-        assert.isFalse(RA.isBigInt(new Error()));
-        assert.isFalse(RA.isBigInt(RA));
-        assert.isFalse(RA.isBigInt(Array.prototype.slice));
-        assert.isFalse(RA.isBigInt({ a: 1 }));
-        assert.isFalse(RA.isBigInt(/x/));
-        assert.isFalse(RA.isBigInt(Object('a')));
-        assert.isFalse(RA.isBigInt('a string'));
-        assert.isFalse(RA.isBigInt(0));
-        assert.isFalse(RA.isBigInt(1));
-        assert.isFalse(RA.isBigInt(-1));
-        assert.isFalse(RA.isBigInt(0.1));
-        assert.isFalse(RA.isBigInt(-0.1));
-        assert.isFalse(RA.isBigInt(Infinity));
-        assert.isFalse(RA.isBigInt(-Infinity));
-        assert.isFalse(RA.isBigInt(9007199254740991));
-        assert.isFalse(RA.isBigInt('9007199254740991'));
-        assert.isFalse(RA.isBigInt(MAX_SAFE_INTEGER));
-        assert.isFalse(RA.isBigInt(MIN_SAFE_INTEGER));
-        assert.isFalse(RA.isBigInt(Number.MAX_VALUE));
-        assert.isFalse(RA.isBigInt(Number.MIN_VALUE));
-        assert.isFalse(RA.isBigInt(NaN));
-        assert.isFalse(RA.isBigInt(Object(0)));
-        assert.isFalse(RA.isBigInt(Object(0.1)));
-
-        if (Symbol !== 'undefined') {
-          assert.isFalse(RA.isBigInt(Symbol));
-        }
-
-        assert.isFalse(RA.isBigInt(null));
-        assert.isFalse(RA.isBigInt(undefined));
-      });
-    });
-
-    it('should support placeholder to specify "gaps"', function () {
-      const isBigInt = RA.isBigInt(R.__);
-
-      assert.isFalse(isBigInt(1));
-    });
   }
+
+  context('given a non-big-integer value', function () {
+    specify('should return false', function () {
+      assert.isFalse(RA.isBigInt(new Date()));
+      assert.isFalse(RA.isBigInt(args));
+      assert.isFalse(RA.isBigInt([1, 2, 3]));
+      assert.isFalse(RA.isBigInt(Object(false)));
+      assert.isFalse(RA.isBigInt(new Error()));
+      assert.isFalse(RA.isBigInt(RA));
+      assert.isFalse(RA.isBigInt(Array.prototype.slice));
+      assert.isFalse(RA.isBigInt({ a: 1 }));
+      assert.isFalse(RA.isBigInt(/x/));
+      assert.isFalse(RA.isBigInt(Object('a')));
+      assert.isFalse(RA.isBigInt('a string'));
+      assert.isFalse(RA.isBigInt(0));
+      assert.isFalse(RA.isBigInt(1));
+      assert.isFalse(RA.isBigInt(-1));
+      assert.isFalse(RA.isBigInt(0.1));
+      assert.isFalse(RA.isBigInt(-0.1));
+      assert.isFalse(RA.isBigInt(Infinity));
+      assert.isFalse(RA.isBigInt(-Infinity));
+      assert.isFalse(RA.isBigInt(9007199254740991));
+      assert.isFalse(RA.isBigInt('9007199254740991'));
+      assert.isFalse(RA.isBigInt(MAX_SAFE_INTEGER));
+      assert.isFalse(RA.isBigInt(MIN_SAFE_INTEGER));
+      assert.isFalse(RA.isBigInt(Number.MAX_VALUE));
+      assert.isFalse(RA.isBigInt(Number.MIN_VALUE));
+      assert.isFalse(RA.isBigInt(NaN));
+      assert.isFalse(RA.isBigInt(Object(0)));
+      assert.isFalse(RA.isBigInt(Object(0.1)));
+
+      if (Symbol !== 'undefined') {
+        assert.isFalse(RA.isBigInt(Symbol));
+      }
+
+      assert.isFalse(RA.isBigInt(null));
+      assert.isFalse(RA.isBigInt(undefined));
+    });
+  });
+
+  it('should support placeholder to specify "gaps"', function () {
+    const isBigInt = RA.isBigInt(R.__);
+
+    assert.isFalse(isBigInt(1));
+  });
 });
