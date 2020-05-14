0c9714ac52f70ff67b3fcd7087aae2cdda325e8b
Felipe Parreira <felipeparreirafb@gmail.com>
2020-05-14 20:20:03 -0300

    refactor(rangeStep): use Math.floor to round numbers

--- src/rangeStep.js
+++ src/rangeStep.js
@@ -1,5 +1,4 @@
-import { curryN, curry, map, range, pipe } from 'ramda';
-import * as R from 'ramda';
+import { curryN, map, range } from 'ramda';
 
 /**
  * Creates a list of numbers that ranges from start to stop according to a given step.
@@ -22,15 +21,8 @@ import * as R from 'ramda';
  * RA.rangeStep(3, -3, -2); // => [3, 1, -1, -3]
  * RA.rangeStep(0, 0, 1); // => [0]
  */
-const nonNegativeOrDefault = curry((value, dft) => (value >= 0 ? value : dft));
-
-const nonNegativeOrMinusOne = nonNegativeOrDefault(R.__, -1);
-
 const rangeStep = curryN(3, (start, stop, step) =>
-  map(
-    (n) => start + step * n,
-    range(0, 1 + pipe(nonNegativeOrMinusOne, Math.trunc)((stop - start) / step))
-  )
+  map((n) => start + step * n, range(0, 1 + Math.floor((stop - start) / step)))
 );
 
 export default rangeStep;
