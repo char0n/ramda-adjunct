5e9bfc933fd57daa76a35ea81152278d2bcbb913
William Blake <gwilliamblake@gmail.com>
2020-05-21 17:15:02 -0500

    feat(src): adds test coverage

--- test/filterIndexed.js
+++ test/filterIndexed.js
@@ -2,12 +2,24 @@ import { assert } from 'chai';
 
 import * as RA from '../src';
 
-describe('filterIndexed', function() {
-  context('given documentation example', function() {
-    specify('should return an array of the value at a given index', function() {
+describe('filterIndexed', function () {
+  context('given documentation example', function () {
+    specify(
+      'should return an array of the value at a given index',
+      function () {
+        assert.deepEqual(
+          RA.filterIndexed((v, idx) => idx === 0, [1, 2, 3]),
+          [1]
+        );
+      }
+    );
+  });
+
+  context('given an empty array as input', function () {
+    specify('should return an empty array', function () {
       assert.deepEqual(
-        RA.filterIndexed((v, idx) => idx === 0, [1, 2, 3]),
-        [1]
+        RA.filterIndexed((v, idx) => idx === 0, []),
+        []
       );
     });
   });
