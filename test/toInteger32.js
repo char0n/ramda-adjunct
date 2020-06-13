f6b8b77edddbc8f5eadcc4bcfaddbb82eefe5f4a
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-06-13 17:32:20 +0200

    Update test/toInteger32.js

--- test/toInteger32.js
+++ test/toInteger32.js
@@ -21,7 +21,7 @@ describe('toInteger32', function () {
     });
   });
 
-  context('given an integer -1 modulo 23^2', function () {
+  context('given an integer 23^2 - 1', function () {
     specify('should return -1', function () {
       const initialValue = 2 ** 32 - 1;
       assert.strictEqual(RA.toInteger32(initialValue), -1);
