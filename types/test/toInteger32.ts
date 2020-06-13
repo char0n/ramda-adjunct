8b2b813058c2af02e9b732a3337f5f3e4dcc0a52
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-06-13 17:28:59 +0200

    Update types/test/toInteger32.ts

--- types/test/toInteger32.ts
+++ types/test/toInteger32.ts
@@ -1,3 +1,13 @@
 import * as RA from 'ramda-adjunct';
 
-RA.toInteger32(6);
+RA.toInteger32(1); // $ExpectType number
+RA.toInteger32(Infinity); // $ExpectType number
+RA.toInteger32(-Infinity); // $ExpectType number
+RA.toInteger32(+Infinity); // $ExpectType number
+RA.toInteger32(NaN); // $ExpectType number
+RA.toInteger32({}); // $ExpectError
+RA.toInteger32('s'); // $ExpectError
+RA.toInteger32([]); // $ExpectError
+
+RA.toInt32(1); // $ExpectType number
+RA.toInt32('s'); // $ExpectError
