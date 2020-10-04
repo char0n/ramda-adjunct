7f957072191c6674a320288323ec3bf7eafb7ec1
Daniel Bartholomae <daniel@bartholomae.name>
2020-10-04 19:10:19 +0200

    test(renameKeyWith): add error cases

--- types/test/renameKeyWith.ts
+++ types/test/renameKeyWith.ts
@@ -4,3 +4,7 @@ RA.renameKeyWith((str) => 'a' + str, 'A', { A: 1 }); // $ExpectType object
 RA.renameKeyWith((str) => 'a' + str)('A', { A: 1 }); // $ExpectType object
 RA.renameKeyWith((str) => 'a' + str)('A')({ A: 1 }); // $ExpectType object
 RA.renameKeyWith((str) => 'a' + str, 'A')({ A: 1 }); // $ExpectType object
+
+RA.renameKeyWith((str) => 1, 'A', { A: 1 }); // $ExpectError
+RA.renameKeyWith((str) => 'a' + str, 1, { A: 1 }); // $ExpectError
+RA.renameKeyWith((str) => 'a' + str, 'A', 1); // $ExpectError
