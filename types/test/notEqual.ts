8c2a0097f1a227d8605015392b55dc8c4cb8ea0c
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-12-13 17:01:36 +0100

    Update notEqual.ts

--- types/test/notEqual.ts
+++ types/test/notEqual.ts
@@ -1,6 +1,7 @@
 import * as RA from 'ramda-adjunct';
 
 RA.notEqual(1, 1); // $ExpectType boolean
+RA.notEqual(1, ''); // $ExpectType boolean
 RA.notEqual('', ''); // $ExpectType boolean
 RA.notEqual([], [1]); // $ExpectType boolean
 RA.notEqual([1, 2, 3, 4], []); // $ExpectType boolean
