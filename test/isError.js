8a4b768c27b2e027a8e81e8f54b6687d7e15ddb0
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-12-15 20:10:23 +0100

    Update isError.js

--- test/isError.js
+++ test/isError.js
@@ -1,6 +1,5 @@
 import { assert } from 'chai';
 import * as R from 'ramda';
-import * as fs from 'fs';
 
 import * as RA from '../src';
 
@@ -8,41 +7,18 @@ describe('isError', function () {
   context('given an error', function () {
     specify('should return true', function () {
       assert.isTrue(RA.isError(new Error()));
+      assert.isTrue(RA.isError(new EvalError()));
       assert.isTrue(RA.isError(new TypeError()));
       assert.isTrue(RA.isError(new RangeError()));
       assert.isTrue(RA.isError(new SyntaxError()));
       assert.isTrue(RA.isError(new ReferenceError()));
-      assert.isTrue(
-        RA.isError(
-          (() => {
-            let error;
-            try {
-              fs.readFileSync('non/existant/file/path.js');
-            } catch (systemError) {
-              error = systemError;
-            }
-            return error;
-          })()
-        )
-      );
-      assert.isTrue(
-        RA.isError(
-          (() => {
-            let error;
-            try {
-              global.assert(1 === 2);
-            } catch (assertionError) {
-              error = assertionError;
-            }
-            return error;
-          })()
-        )
-      );
+      assert.isTrue(RA.isError(new URIError()));
     });
   });
 
   context('given a non-error value', function () {
     specify('should return false', function () {
+      assert.isFalse(RA.isError(Error));
       assert.isFalse(RA.isError('I am not an error'));
       assert.isFalse(RA.isError([1, 2, 3]));
       assert.isFalse(RA.isError(new Set([1, 2, 3])));
@@ -59,6 +35,5 @@ describe('isError', function () {
     const isError = RA.isError(R.__);
 
     assert.isFalse(isError(1));
-    assert.isTrue(isError(new Error()));
   });
 });
