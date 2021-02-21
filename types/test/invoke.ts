bad6974c592680be8db35766f2b28aed4f26d707
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-02-21 10:29:52 +0100

    Update invoke.ts

--- types/test/invoke.ts
+++ types/test/invoke.ts
@@ -1,6 +1,6 @@
 import * as RA from 'ramda-adjunct';
 
-RA.invoke(['abs'], Math); // $ExpectType any
+RA.invoke(['random'], Math); // $ExpectType any
 RA.invoke(['nonexistentMethod'], Math); // $ExpectType any
 RA.invoke([0, 'a'], Math); // $ExpectError
 RA.invoke([{}], Math); // $ExpectError
