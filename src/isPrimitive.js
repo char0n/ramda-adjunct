93837181517564bf79f3b3113a7d390952feb2fe
Brandon Maushund <maushundb@gmail.com>
2021-02-27 13:12:07 -0500

    Update src/isPrimitive.js
    Co-authored-by: Vladimir Gorej <vladimir.gorej@gmail.com>

--- src/isPrimitive.js
+++ src/isPrimitive.js
@@ -17,6 +17,7 @@ import isSymbol from './isSymbol';
  * @func isPrimitive
  * @category Type
  * @sig * -> Boolean
+ * @since {@link https://char0n.github.io/ramda-adjunct/2.31.0|v2.31.0}
  * @param {*} val The value to test
  * @return {boolean}
  * @see {@link RA.isNotPrimitive|isNotPrimitive}, {@link RA.isPrimitive|isPrimitive}, {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#primitive_values|MDN Primitive values}, {@link https://developer.mozilla.org/en-US/docs/Glossary/Primitive|MDN Primitive}
