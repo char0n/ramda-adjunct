fc29380a17b4f943bb4e9fa2c37293ff6fc22a17
Brandon Maushund <maushundb@gmail.com>
2021-02-27 13:03:08 -0500

    Update src/isNotPrimitive.js
    Co-authored-by: Vladimir Gorej <vladimir.gorej@gmail.com>

--- src/isNotPrimitive.js
+++ src/isNotPrimitive.js
@@ -8,6 +8,7 @@ import isPrimitive from './isPrimitive';
  * @func isNotPrimitive
  * @category Type
  * @sig * -> Boolean
+ * @since {@link https://char0n.github.io/ramda-adjunct/2.31.0|v2.31.0}
  * @param {*} val The value to test
  * @return {boolean}
  * @see {@link RA.isNotObj|isNotObj}, {@link RA.isPrimitive|isPrimitive}
