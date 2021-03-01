d674211eff6697a6e1381c234eb4ea1ea649ba9e
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-03-01 22:00:05 +0100

    Update src/isNotPrimitive.js

--- src/isNotPrimitive.js
+++ src/isNotPrimitive.js
@@ -8,7 +8,7 @@ import isPrimitive from './isPrimitive';
  * @func isNotPrimitive
  * @category Type
  * @sig * -> Boolean
- * @since {@link https://char0n.github.io/ramda-adjunct/2.31.0|v2.31.0}
+ * @since {@link https://char0n.github.io/ramda-adjunct/2.32.0|v2.32.0}
  * @param {*} val The value to test
  * @return {boolean}
  * @see {@link RA.isPrimitive|isPrimitive}, {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#primitive_values|MDN Primitive values}, {@link https://developer.mozilla.org/en-US/docs/Glossary/Primitive|MDN Primitive}
