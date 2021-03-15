b49829ffc4975b63f2c2b0e983008df05dbffe66
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-03-15 21:18:44 +0100

    Update findOr.ts

--- types/test/findOr.ts
+++ types/test/findOr.ts
@@ -5,11 +5,10 @@ RA.findOr('1', val => val === '1', ['1', '2', '3']); // $ExpectType string
 RA.findOr('1', val => val === 4, [1, 2, 3]); // $ExpectType number | "1"
 RA.findOr(1, () => false, [1, 2, 3]); // $ExpectType number
 
-RA.findOr('1')(val => val === 4, [1, 2, 3]); // $ExpectType number | "1"
-RA.findOr('1', val => val === 4)([1, 2, 3]); // $ExpectType number | "1"
-RA.findOr('1')(val => val === 4)([1, 2, 3]); // $ExpectType number | "1"
+RA.findOr<'1', number>('1')(val => val === 4, [1, 2, 3]); // $ExpectType number | "1"
+RA.findOr<'1', number>('1', val => val === 4)([1, 2, 3]); // $ExpectType number | "1"
+RA.findOr<'1', number>('1')(val => val === 4)([1, 2, 3]); // $ExpectType number | "1"
 
 RA.findOr(1, val => val, [1, 2, 3]); // $ExpectError
 RA.findOr(1, val => val); // $ExpectError
-RA.findOr(1); // $ExpectError
 RA.findOr(); // $ExpectError
