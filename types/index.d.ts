6c9ac3febb12b30c78d52f73865285eb7a3fdb2d
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-04-13 15:04:58 +0200

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -456,7 +456,7 @@ declare namespace RamdaAdjunct {
         isSparseArray(val: any): boolean;
 
         /**
-         * Checks whether the passed value is 
+         * Checks whether the passed value is
          * {@link https://github.com/getify/You-Dont-Know-JS/blob/9959fc904d584bbf0b02cf41c192f74ff4238581/types-grammar/ch4.md#the-curious-case-of-the-|a sentinel value}.
          */
         isSentinelValue(val: any): boolean;
