2d5496f944126bb01e0208de57ea568af2e46d6f
Vladimir Gorej <vladimir.gorej@gmail.com>
2021-11-01 21:16:51 +0100

    Update copyKeys.js

--- test/copyKeys.js
+++ test/copyKeys.js
@@ -17,20 +17,20 @@ describe('copyKeys', function () {
       }
     );
   });
-  
+
   context('given non existing keys', function () {
     specify('should ignore them', function () {
       assert.deepEqual(RA.copyKeys({ nonExistingKey: 'key2' }, { key1: 1 }), {
         key1: 1,
-      });      
-    });  
-    
+      });
+    });
+
     specify('should create shallow clone', function () {
       const obj = { key1: 1 };
-      
-      assert.notStrictEqual(RA.copyKeys({ nonExistingKey: 'key2' }, obj), obj);      
-    });  
-  });  
+
+      assert.notStrictEqual(RA.copyKeys({ nonExistingKey: 'key2' }, obj), obj);
+    });
+  });
 
   context('given non object value', function () {
     specify('should return empty object', function () {
@@ -39,8 +39,8 @@ describe('copyKeys', function () {
       assert.deepEqual(RA.copyKeys({ key1: 'key2' }, ''), {});
       assert.deepEqual(RA.copyKeys({ key1: 'key2' }, 3), {});
     });
-  });  
-  
+  });
+
   it('should be curried', function () {
     assert.deepEqual(
       RA.copyKeys({ key1: 'key2', key2: 'key3' }, { key1: 1, key2: 2 }),
