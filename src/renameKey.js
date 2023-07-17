1c1cd3d258ed1f22e079c4a7750184097c0e2e51
nolleto <felipenolletonascimento@gmail.com>
2023-07-17 11:54:52 -0300

    Update `renameKey` implementation

--- src/renameKey.js
+++ src/renameKey.js
@@ -1,25 +1,24 @@
-import { always, curry, equals, when } from 'ramda';
+import { curry } from 'ramda';
 
-import renameKeysWith from './renameKeysWith';
+import renameKeys from './renameKeys';
 
 /**
  * Creates a new object with the own properties of the provided object, but a
- * single key is renamed according to the currentKey as `'oldKey'` and newKey as `'newKey'`.
- * When some key is not found in the currentKey, then it's passed as-is.
+ * single key is renamed from `oldKey` to `newKey`.
  *
- * Keep in mind that in the case of keys conflict is behaviour undefined and
+ * Keep in mind that in the case of keys conflict is behavior undefined and
  * the result may vary between various JS engines!
  *
  * @func renameKey
  * @memberOf RA
- * @since {@link https://char0n.github.io/ramda-adjunct/1.5.0|v1.5.0}
+ * @since {@link https://char0n.github.io/ramda-adjunct/4.1.0|v4.1.0}
  * @category Object
- * @sig [String a] -> [String b] -> {[String a]: *} -> {[String b]: *}
- * @param {!string} currentKey
+ * @sig (String a, String b) => a -> b ->  {a: *} -> {b: *}
+ * @param {!string} oldKey
  * @param {!string} newKey
  * @param {!Object} obj
  * @return {!Object} New object with renamed key
- * @see {@link https://github.com/ramda/ramda/wiki/Cookbook#rename-key-of-an-object|Ramda Cookbook}, {@link RA.renameKey|renameKey}
+ * @see {@link https://github.com/ramda/ramda/wiki/Cookbook#rename-key-of-an-object|Ramda Cookbook}, {@link RA.renameKeyWith|renameKeyWith}
  * @example
  *
  * const input = { firstName: 'Elisia', age: 22, type: 'human' };
@@ -27,10 +26,8 @@ import renameKeysWith from './renameKeysWith';
  * RA.renameKey('firstName', 'name')(input);
  * //=> { name: 'Elisia', age: 22, type: 'human' }
  */
-const renameKey = curry((currentKey, newKey, obj) => {
-  const replaceCurrentKeyWithNewKey = when(equals(currentKey), always(newKey));
-
-  return renameKeysWith(replaceCurrentKeyWithNewKey, obj);
-});
+const renameKey = curry((oldKey, newKey, obj) =>
+  renameKeys({ [oldKey]: newKey }, obj)
+);
 
 export default renameKey;
